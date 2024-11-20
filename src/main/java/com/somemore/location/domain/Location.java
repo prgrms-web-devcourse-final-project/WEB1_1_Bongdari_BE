4938cb83b1695387737ce9ec7395d6d6769e2874
leebs0521 <leebs0521@gmail.com>
2024-11-20 22:32:00 +0900

    feat(location): Location 엔티티, 레포지토리 추가

---
+++ src/main/java/com/somemore/location/domain/Location.java
@@ -0,0 +1,40 @@
+package com.somemore.location.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+@Table(name = "location")
+public class Location {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "latitude", nullable = false)
+    private String latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private String longitude;
+
+    @Builder
+    public Location(String address, String latitude, String longitude) {
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
