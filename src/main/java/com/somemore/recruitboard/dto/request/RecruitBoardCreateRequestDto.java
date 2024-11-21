32c660e9dcce6343928e069dd4c76fdf73ed63a7
Beom-Su Lee <leebs0521@gmail.com>
2024-11-21 20:06:46 +0900

    feat: recruit board 생성 기능 수정 (#23)
    * feat(recruit-board): RecruitBoard 엔티티 수정
     - 필드 추가 및 수정
     - 메서드 추가
    
    * feat(recruit-board): RecruitBoardCreateRequestDto 수정
     - 필드 추가 및 수정
    
    * test(recruit-board): 변경에 따른 테스트 수정 및 테스트 케이스 추가
    
    * refactor(recruit-board): 테스트가 모호한 부분 수정

--- src/main/java/com/somemore/recruitboard/dto/request/RecruitBoardCreateRequestDto.java
+++ src/main/java/com/somemore/recruitboard/dto/request/RecruitBoardCreateRequestDto.java
@@ -8,9 +8,7 @@ import com.somemore.recruitboard.domain.VolunteerType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.UUID;
 import lombok.Builder;
 
@@ -29,24 +27,22 @@ public record RecruitBoardCreateRequestDto(
     @Schema(description = "예상 모집 인원", example = "4")
     @NotNull(message = "예상 모집 인원은 필수 값입니다.")
     Integer recruitmentCount,
-    @Schema(description = "봉사 일시", example = "2024-11-20T10:00:00")
-    @NotNull(message = "봉사 일시는 필수 값입니다.")
-    LocalDateTime volunteerDate,
+    @Schema(description = "봉사 시작 일시", example = "2024-11-20T10:00:00")
+    @NotNull(message = "봉사 시작 일시는 필수 값입니다.")
+    LocalDateTime volunteerStartDateTime,
+    @Schema(description = "봉사 종료 일시", example = "2024-11-20T12:00:00")
+    @NotNull(message = "봉사 종료 일시는 필수 값입니다.")
+    LocalDateTime volunteerEndDateTime,
     @Schema(description = "봉사 활동 유형", example = "ENVIRONMENTAL_PROTECTION")
     @NotNull(message = "봉사 활동 유형은 필수 값입니다.")
     VolunteerType volunteerType,
-    @Schema(description = "봉사 시간(시)", example = "1")
-    @Positive(message = "봉사 시간(시)은 1이상 이어야 합니다.")
-    Integer volunteerHours,
-    @Schema(description = "봉사 시간(분)", example = "30")
-    @Positive(message = "봉사 시간(분)은 1이상 이어야 합니다.")
-    Integer volunteerMinutes,
     @Schema(description = "봉사 시간 인정 여부", example = "true")
     @NotNull(message = "시간 인정 여부는 필수 값입니다.")
     Boolean admitted,
     @NotNull(message = "위치 정보는 필수 값입니다.")
     LocationCreateRequestDto location
-){
+) {
+
     public RecruitBoard toEntity(UUID centerId, Long locationId, String imgUrl) {
         return RecruitBoard.builder()
             .centerId(centerId)
@@ -56,9 +52,9 @@ public record RecruitBoardCreateRequestDto(
             .region(region)
             .recruitmentCount(recruitmentCount)
             .imgUrl(imgUrl)
-            .volunteerDate(volunteerDate)
+            .volunteerStartDateTime(volunteerStartDateTime)
+            .volunteerEndDateTime(volunteerEndDateTime)
             .volunteerType(volunteerType)
-            .volunteerHours(LocalTime.of(volunteerHours, volunteerMinutes))
             .admitted(admitted)
             .build();
     }
--- src/test/java/com/somemore/recruitboard/domain/RecruitBoardTest.java
+++ src/test/java/com/somemore/recruitboard/domain/RecruitBoardTest.java
@@ -3,18 +3,21 @@ package com.somemore.recruitboard.domain;
 import static com.somemore.recruitboard.domain.RecruitStatus.RECRUITING;
 import static com.somemore.recruitboard.domain.VolunteerType.OTHER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RecruitBoardTest {
 
     @DisplayName("봉사 모집글 생성시 모집상태는 모집중이다")
     @Test
-    void CreateRecruitBoardWithDefaultStatus() {
+    void createRecruitBoardWithDefaultStatus() {
         // given
         RecruitBoard board = RecruitBoard.builder()
             .centerId(UUID.randomUUID())
@@ -24,9 +27,9 @@ class RecruitBoardTest {
             .region("경기")
             .recruitmentCount(10)
             .imgUrl("https://image.domain.com/links")
-            .volunteerDate(LocalDateTime.now())
+            .volunteerStartDateTime(LocalDateTime.now())
+            .volunteerEndDateTime(LocalDateTime.now().plusHours(1))
             .volunteerType(OTHER)
-            .volunteerHours(LocalTime.of(7, 30))
             .admitted(true)
             .build();
 
@@ -35,5 +38,60 @@ class RecruitBoardTest {
 
         // then
         assertThat(recruitStatus).isEqualTo(RECRUITING);
+    }
+
+    @DisplayName("봉사 종료 시간이 시작 시간과 같거나 빠르면, 봉사 모집글 생성 시 에러가 발생한다")
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1})
+    void createRecruitBoardWithInValidVolunteerTime(long secondsOffset) {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime endDateTime = now.plusSeconds(secondsOffset);
+
+        // when & then
+        assertThatThrownBy(
+            () -> RecruitBoard.builder()
+                .centerId(UUID.randomUUID())
+                .locationId(1L)
+                .title("봉사모집제목")
+                .content("봉사모집내용")
+                .region("경기")
+                .recruitmentCount(10)
+                .imgUrl("https://image.domain.com/links")
+                .volunteerStartDateTime(now)
+                .volunteerEndDateTime(endDateTime)
+                .volunteerType(VolunteerType.OTHER)
+                .admitted(true)
+                .build()
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("봉사 시간을 계산할 수 있다")
+    @Test
+    void testCalculateVolunteerTime() {
+        // given
+        int hours = 3;
+        LocalDateTime startDateTime = LocalDateTime.now();
+        LocalDateTime endDateTime = startDateTime.plusHours(hours);
+
+        RecruitBoard board = RecruitBoard.builder()
+            .centerId(UUID.randomUUID())
+            .locationId(1L)
+            .title("봉사모집제목")
+            .content("봉사모집내용")
+            .region("경기")
+            .recruitmentCount(10)
+            .imgUrl("https://image.domain.com/links")
+            .volunteerStartDateTime(startDateTime)
+            .volunteerEndDateTime(endDateTime)
+            .volunteerType(OTHER)
+            .admitted(true)
+            .build();
+
+        // when
+        LocalTime volunteerTime = board.calculateVolunteerTime();
+
+        // then
+        assertThat(volunteerTime).isEqualTo(LocalTime.of(hours, 0));
     }
 }
