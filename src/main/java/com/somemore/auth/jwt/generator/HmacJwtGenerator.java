064117926fce898e4f537a69713869d9a2405096
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-23 20:56:35 +0900

    feat(JwtGenerator): HmacJwtGenerator 개선 및 EncodedToken 반환 방식으로 변경
    - JWT 생성 로직 수정:
      - 기존 String 반환 방식에서 EncodedToken 객체 반환 방식으로 변경
      - TokenType을 사용하여 토큰 유효 기간 동적으로 설정 가능 (ACCESS_TOKEN, REFRESH_TOKEN 구분)
    - 클레임(Claims) 생성 방식 개선:
      - buildClaims 메서드로 사용자 ID와 역할(role) 추가

--- src/main/java/com/somemore/auth/jwt/generator/HmacJwtGenerator.java
+++ src/main/java/com/somemore/auth/jwt/generator/HmacJwtGenerator.java
@@ -1,5 +1,8 @@
 package com.somemore.auth.jwt.generator;
 
+import com.somemore.auth.jwt.domain.EncodedToken;
+import com.somemore.auth.jwt.domain.TokenType;
+import com.somemore.auth.jwt.refresh.manager.RefreshTokenManager;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.MacAlgorithm;
@@ -14,40 +17,29 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class HmacJwtGenerator implements JwtGenerator {
 
-    private static final String ID = "id";
-    private static final String ROLE = "role";
-    private static final long ACCESS_TOKEN_PERIOD = 1000 * 60 * 60 * 24 * 7; // 1주
-    private static final long REFRESH_TOKEN_PERIOD = 1000 * 60 * 30; // 30분
-
     public static final MacAlgorithm ALGORITHM = Jwts.SIG.HS256;
-
     private final SecretKey secretKey;
 
-    public String generateToken(String userId, String role) {
-        String accessToken = generateToken(userId, role, ACCESS_TOKEN_PERIOD);
-        String refreshToken = generateToken(userId, role, REFRESH_TOKEN_PERIOD);
-
-        // TODO tokenService.saveRefreshToken(userId, accessToken, refreshToken);
-        return accessToken;
-    }
-
-    private String generateToken(String userId, String role, long period) {
+    public EncodedToken generateToken(String userId, String role, TokenType tokenType) {
         Claims claims = buildClaims(userId, role);
         Instant now = Instant.now();
-        Instant expirationInstant = now.plusMillis(period);
+        Instant expiration = now.plusMillis(tokenType.getPeriod());
 
-        return Jwts.builder()
+        return new EncodedToken(Jwts.builder()
                 .claims(claims)
                 .issuedAt(Date.from(now))
-                .expiration(Date.from(expirationInstant))
+                .expiration(Date.from(expiration))
                 .signWith(secretKey, ALGORITHM)
-                .compact();
+                .compact());
     }
 
     private static Claims buildClaims(String userId, String role) {
-        Claims claims = Jwts.claims().build();
-        claims.put(ID, userId);
-        claims.put(ROLE, role);
-        return claims;
+        final String ID = "id";
+        final String ROLE = "role";
+
+        return Jwts.claims()
+                .add(ID, userId)
+                .add(ROLE, role)
+                .build();
     }
 }
