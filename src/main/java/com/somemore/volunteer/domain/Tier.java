5f839374c560e5b8cb9579fc6cfe09f8babf0d7b
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-23 19:26:06 +0900

    feat(Tier): 불필요한 rank 필드 삭제

--- src/main/java/com/somemore/volunteer/domain/Tier.java
+++ src/main/java/com/somemore/volunteer/domain/Tier.java
@@ -4,19 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum Tier {
-    RED(1),
-    ORANGE(2),
-    YELLOW(3),
-    GREEN(4),
-    BLUE(5),
-    INDIGO(6),
-    VIOLET(7),
-    RAINBOW(8);
-
-    private final int rank;
-
-    Tier(int rank) {
-        this.rank = rank;
-    }
-
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    INDIGO,
+    VIOLET,
+    RAINBOW;
 }
