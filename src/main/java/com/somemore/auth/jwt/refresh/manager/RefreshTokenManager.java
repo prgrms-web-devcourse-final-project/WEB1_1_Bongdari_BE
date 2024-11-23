7d046a1614606748c100221b6490f0087417117e
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-23 21:19:31 +0900

    feat(RefreshToken): Redis 기반 RefreshToken 관리 기능 구현
    - RefreshToken 엔티티 추가:
      - @RedisHash 어노테이션으로 Redis에 저장
      - userId(@Id)를 Redis 키로 사용하여 유저별 고유 RefreshToken 관리
      - TTL(Time-to-Live) 설정: 7일
      - accessToken 갱신 메서드 추가
    
    - 매니저 주요 기능:
      - AccessToken으로 RefreshToken 조회
      - RefreshToken 저장
      - RefreshToken 삭제 (로그아웃 구현 예정)

---
+++ src/main/java/com/somemore/auth/jwt/refresh/manager/RefreshTokenManager.java
@@ -0,0 +1,12 @@
+package com.somemore.auth.jwt.refresh.manager;
+
+import com.somemore.auth.jwt.domain.EncodedToken;
+import com.somemore.auth.jwt.refresh.domain.RefreshToken;
+
+public interface RefreshTokenManager {
+    RefreshToken findRefreshToken(EncodedToken accessToken);
+
+    void save(RefreshToken refreshToken);
+
+    void removeRefreshToken(EncodedToken accessToken);
+}
