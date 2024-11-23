2f1e8a88900f70b5df47bf041f75ed07c5ca1a30
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-23 23:10:57 +0900

    feat(config): Spring Security 설정 추가 및 OAuth2 인증 처리 구성
    - Stateless 세션 관리 설정(SessionCreationPolicy.STATELESS)
    - CSRF, HTTP Basic, Form Login, Logout 기능 비활성화
    
    - SecurityFilterChain 설정:
      - 공개 API 경로 설정
      - 그 외 모든 요청 인증 필요
    
    - OAuth2 인증 처리:
      - CustomOAuth2UserService로 사용자 정보 처리
      - CustomOAuthSuccessHandler로 인증 성공 처리
      - CustomOAuthFailureHandler로 인증 실패 처리
    
    - TODO:
      - JWT 인증 필터 추가
      - JWT 예외 필터 추가

--- src/main/java/com/somemore/global/configure/SecurityConfig.java
+++ src/main/java/com/somemore/global/configure/SecurityConfig.java
@@ -1,4 +1,67 @@
 package com.somemore.global.configure;
 
+import com.somemore.auth.oauth.handler.failure.CustomOAuthFailureHandler;
+import com.somemore.auth.oauth.handler.success.CustomOAuthSuccessHandler;
+import com.somemore.auth.oauth.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@Slf4j
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOAuthSuccessHandler customOAuthSuccessHandler;
+    private final CustomOAuthFailureHandler customOAuthFailureHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers(
+                                        "/login",
+                                        "/oauth2/**",
+                                        "/api/auth/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger/**",
+                                        "/swagger-ui.html",
+                                        "/swagger-ui/**")
+                                .permitAll()
+                                .anyRequest().authenticated()
+                )
+
+                .oauth2Login(oauth2 ->
+                        oauth2
+                                .userInfoEndpoint(userInfoEndpointConfig ->
+                                        userInfoEndpointConfig.userService(customOAuth2UserService))
+                                .failureHandler(customOAuthFailureHandler)
+                                .successHandler(customOAuthSuccessHandler)
+                ).build();
+
+
+//        // TODO JWT 인증 필터가 인증 요청 처리, JWT 인증 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
+//        return httpSecurity
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(jwtExceptionFilter, JwtAuthFilter.class)
+//                .build();
+    }
 }
