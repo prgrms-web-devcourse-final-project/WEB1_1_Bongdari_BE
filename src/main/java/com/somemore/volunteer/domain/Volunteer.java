945f6f9a010c82a630ab8f7efb4d200ac8e7e177
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-23 19:49:37 +0900

    feat(Volunteer): Volunteer 엔티티 추가
    - BaseEntity 상속
    - 필드:
      - id: UUID (BINARY(16), 고유 식별자)
      - oauthProvider: OAuth 인증 제공자 (EnumType.STRING)
      - oauthId: OAuth 제공자 ID
      - tier: 자원봉사 티어 (EnumType.STRING)
    
    - 정적 메서드를 통해 초기 값이 설정된 Volunteer 객체 생성 가능

---
+++ src/main/java/com/somemore/volunteer/domain/Volunteer.java
@@ -0,0 +1,81 @@
+package com.somemore.volunteer.domain;
+
+import com.somemore.auth.oauth.OAuthProvider;
+import com.somemore.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "volunteer")
+public class Volunteer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "oauth_provider", nullable = false)
+    private OAuthProvider oauthProvider;
+
+    @Column(name = "oauth_id", nullable = false)
+    private String oauthId;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "img_url", nullable = false)
+    private String imgUrl;
+
+    @Lob
+    @Column(name = "introduce", nullable = false)
+    private String introduce;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tier", nullable = false, length = 20)
+    private Tier tier;
+
+    @Column(name = "total_volunteer_hours", nullable = false)
+    private Integer totalVolunteerHours;
+
+    @Column(name = "total_volunteer_count", nullable = false)
+    private Integer totalVolunteerCount;
+
+
+    public static Volunteer createDefault(OAuthProvider oauthProvider, String oauthId) {
+        return Volunteer.builder()
+                .oauthProvider(oauthProvider)
+                .oauthId(oauthId)
+                .nickname(UUID.randomUUID().toString().substring(0, 8))
+                .imgUrl("")
+                .introduce("")
+                .tier(Tier.RED)
+                .totalVolunteerHours(0)
+                .totalVolunteerCount(0)
+                .build();
+    }
+
+    @Builder
+    private Volunteer(
+            OAuthProvider oauthProvider,
+            String oauthId,
+            String nickname,
+            String imgUrl,
+            String introduce,
+            Tier tier,
+            Integer totalVolunteerHours,
+            Integer totalVolunteerCount
+    ) {
+        this.oauthProvider = oauthProvider;
+        this.oauthId = oauthId;
+        this.nickname = nickname;
+        this.imgUrl = imgUrl;
+        this.introduce = introduce;
+        this.tier = tier;
+        this.totalVolunteerHours = totalVolunteerHours;
+        this.totalVolunteerCount = totalVolunteerCount;
+    }
+}
