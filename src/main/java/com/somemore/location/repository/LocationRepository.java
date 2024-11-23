92b1982436bbabc9cf38727417ad19644b5311ee
Beom-Su Lee <leebs0521@gmail.com>
2024-11-23 13:21:11 +0900

    feat: QueryDsl 의존성 추가 (#28)
    * build(query-dsl): QueryDsl 의존성 추가
    
    * chore: QueryDsl Q클래스 .gitignore 추가
    
    * feat: QueryDsl Config 추가
    
    * feat: location 도메인 queryDsl 적용
    
    * chore: 불필요한 import 제거
    
    * chore: 개행 추가

--- src/main/java/com/somemore/location/repository/LocationRepository.java
+++ src/main/java/com/somemore/location/repository/LocationRepository.java
@@ -1,8 +1,13 @@
 package com.somemore.location.repository;
 
 import com.somemore.location.domain.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository {
 
+    Location save(Location location);
+
+    Optional<Location> findById(Long id);
+
+    void deleteAllInBatch();
 }
