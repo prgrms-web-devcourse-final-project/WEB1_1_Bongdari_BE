f38d9938679457c027a09a762c16c2793265e3fc
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-23 23:09:21 +0900

    feat(GenerateTokensOnLogin): 로그인 시 AccessToken 및 RefreshToken 생성 로직 구현

---
+++ src/main/java/com/somemore/auth/jwt/service/command/GenerateTokensOnLoginService.java
@@ -0,0 +1,54 @@
+package com.somemore.auth.jwt.service.command;
+
+import com.somemore.auth.jwt.domain.UserRole;
+import com.somemore.auth.jwt.domain.EncodedToken;
+import com.somemore.auth.jwt.domain.TokenType;
+import com.somemore.auth.jwt.generator.JwtGenerator;
+import com.somemore.auth.jwt.refresh.domain.RefreshToken;
+import com.somemore.auth.jwt.refresh.manager.RefreshTokenManager;
+import com.somemore.auth.jwt.usecase.command.GenerateTokensOnLoginUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class GenerateTokensOnLoginService implements GenerateTokensOnLoginUseCase {
+
+    private final JwtGenerator jwtGenerator;
+    private final RefreshTokenManager refreshTokenManager;
+
+    @Override
+    public EncodedToken saveRefreshTokenAndReturnAccessToken(UUID volunteerId) {
+        EncodedToken accessToken = generateToken(volunteerId, TokenType.ACCESS);
+        RefreshToken refreshToken = generateRefreshToken(volunteerId, accessToken);
+        saveRefreshToken(refreshToken);
+
+        return accessToken;
+    }
+
+    private EncodedToken generateToken(UUID volunteerId, TokenType tokenType) {
+        return jwtGenerator.generateToken(
+                volunteerId.toString(),
+                UserRole.VOLUNTEER.name(),
+                tokenType);
+    }
+
+    private RefreshToken generateRefreshToken(UUID volunteerId, EncodedToken accessToken) {
+        return new RefreshToken(
+                volunteerId.toString(),
+                accessToken,
+                generateToken(
+                        volunteerId,
+                        TokenType.REFRESH));
+    }
+
+    private void saveRefreshToken(RefreshToken refreshToken) {
+        refreshTokenManager.save(refreshToken);
+    }
+}
