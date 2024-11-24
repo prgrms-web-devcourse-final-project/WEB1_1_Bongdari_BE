6e9aecb4822bd9f47e6349c567aae92d5efb6715
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-25 01:30:12 +0900

    style(개행): git 잠재적인 에러 예방

--- src/main/java/com/somemore/volunteer/repository/VolunteerDetailRepository.java
+++ src/main/java/com/somemore/volunteer/repository/VolunteerDetailRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface VolunteerDetailRepository extends JpaRepository<VolunteerDetail, Long> {
+
     Optional<VolunteerDetail> findByVolunteerId(UUID volunteerId);
 }
