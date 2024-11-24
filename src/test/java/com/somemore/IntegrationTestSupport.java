189c7c91487d955db7e11433ba83eb5dd2a031c7
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-24 21:57:02 +0900

    fix(IntegrationTestSupport): @JpaAuditing 설정을 prod 환경으로 이동
    - 테스트 환경과 프로덕션 환경에서 중복된 @JpaAuditing 설정으로 발생한 문제 해결
    - @JpaAuditing 활성화를 프로덕션 환경으로 제한하여 충돌 방지

--- src/test/java/com/somemore/IntegrationTestSupport.java
+++ src/test/java/com/somemore/IntegrationTestSupport.java
@@ -1,11 +1,9 @@
 package com.somemore;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@EnableJpaAuditing
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 
