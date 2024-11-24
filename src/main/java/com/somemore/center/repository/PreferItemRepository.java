e8db6e037715f8510b4e46eaf0133dc66912e1f7
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-11-24 17:47:59 +0900

    Fix: 코드 리뷰 사항 반영
    - 서비스 레이어의 public 메서드를 상단에 배치하여 가독성 향상
    - CQRS 적용을 위한 Query, Command 서비스 클래스에 @Transactinal(readOnly = true), @Transactinal 어노테이션을 각각 추가
    - 레포지토리의 불필요한 @Query 어노테이션 삭제

--- src/main/java/com/somemore/center/repository/PreferItemRepository.java
+++ src/main/java/com/somemore/center/repository/PreferItemRepository.java
@@ -2,7 +2,6 @@ package com.somemore.center.repository;
 
 import com.somemore.center.domain.PreferItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +11,6 @@ import java.util.UUID;
 @Repository
 public interface PreferItemRepository extends JpaRepository<PreferItem, Long> {
 
-    @Query("SELECT p FROM PreferItem p WHERE p.centerId = :centerId")
     List<PreferItem> findByCenterId(@Param("centerId") UUID centerId);
 
 }
--- src/main/java/com/somemore/center/service/query/PreferItemQueryService.java
+++ src/main/java/com/somemore/center/service/query/PreferItemQueryService.java
@@ -5,11 +5,13 @@ import com.somemore.center.repository.PreferItemRepository;
 import com.somemore.center.usecase.query.PreferItemQueryUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class PreferItemQueryService implements PreferItemQueryUseCase {
 
