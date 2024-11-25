455fe76aba301f1c1cc39dcbfac623250462c9e8
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-25 10:35:43 +0900

    fix(Test): 중복된 테스트 설정 정리

--- src/test/java/com/somemore/volunteer/service/query/FindVolunteerIdServiceTest.java
+++ src/test/java/com/somemore/volunteer/service/query/FindVolunteerIdServiceTest.java
@@ -9,14 +9,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
 class FindVolunteerIdServiceTest extends IntegrationTestSupport {
 
     @Autowired
