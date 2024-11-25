27f328d6e5b1a4370af766d4aad7c30348f70f23
ayoung-dev <dkdudab@hanmail.net>
2024-11-25 22:15:20 +0900

    feat(volunteer): volunteer Query Usecase 및 Service에 volunteer 정보 조회 기능 추가

--- src/main/java/com/somemore/volunteer/usecase/query/FindVolunteerIdUseCase.java
+++ src/main/java/com/somemore/volunteer/usecase/query/FindVolunteerIdUseCase.java
@@ -1,7 +1,11 @@
 package com.somemore.volunteer.usecase.query;
 
+import com.somemore.volunteer.dto.response.VolunteerForCommunityResponseDto;
+
 import java.util.UUID;
 
 public interface FindVolunteerIdUseCase {
     UUID findVolunteerIdByOAuthId(String oAuthId);
+    String getNicknameById(UUID id);
+    VolunteerForCommunityResponseDto getVolunteerDetailForCommunity(UUID id);
 }
