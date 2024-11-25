8d0fd8d93a5d7c67fc16f05e4e8a29b6dcfd4ef3
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-25 10:37:04 +0900

    refactor(OAuthResponseConverter): private 기본 생성자를 어노테이션으로 대체

--- src/main/java/com/somemore/auth/oauth/naver/util/OAuthResponseConverter.java
+++ src/main/java/com/somemore/auth/oauth/naver/util/OAuthResponseConverter.java
@@ -2,14 +2,14 @@ package com.somemore.auth.oauth.naver.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.somemore.auth.oauth.naver.dto.response.NaverUserProfileResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Map;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class OAuthResponseConverter {
-
-    private OAuthResponseConverter(){
-    }
 
     public static NaverUserProfileResponseDto convertToNaverUserProfileResponseDto(OAuth2User oAuth2User) {
         ObjectMapper objectMapper = new ObjectMapper();
