ffcafa19ece6ce83150b8cd1d6eb68f7eec0b78b
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-25 11:52:57 +0900

    refactor(ProcessNaverOAuthUserService): @Component -> @Service

--- src/main/java/com/somemore/auth/oauth/naver/service/query/ProcessNaverOAuthUserService.java
+++ src/main/java/com/somemore/auth/oauth/naver/service/query/ProcessNaverOAuthUserService.java
@@ -1,20 +1,20 @@
 package com.somemore.auth.oauth.naver.service.query;
 
 import com.somemore.auth.oauth.OAuthProvider;
+import com.somemore.auth.oauth.naver.dto.response.NaverUserProfileResponseDto;
 import com.somemore.auth.oauth.naver.usecase.query.CheckNaverUserUseCase;
 import com.somemore.auth.oauth.usecase.ProcessOAuthUserUseCase;
-import com.somemore.auth.oauth.naver.dto.response.NaverUserProfileResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.somemore.auth.oauth.naver.util.OAuthResponseConverter.convertToNaverUserProfileResponseDto;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ProcessNaverOAuthUserService implements ProcessOAuthUserUseCase {
