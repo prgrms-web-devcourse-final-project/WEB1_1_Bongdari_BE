a32242a79cb41915f87b14982e08b6f03511f5a3
leebs0521 <leebs0521@gmail.com>
2024-11-25 14:51:19 +0900

    test(recruit-board): RecruitBoard 수정 기능 테스트

--- src/test/java/com/somemore/recruitboard/domain/RecruitBoardTest.java
+++ src/test/java/com/somemore/recruitboard/domain/RecruitBoardTest.java
@@ -4,6 +4,7 @@ import static com.somemore.recruitboard.domain.RecruitStatus.RECRUITING;
 import static com.somemore.recruitboard.domain.VolunteerType.OTHER;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.somemore.recruitboard.dto.request.RecruitBoardUpdateRequestDto;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.UUID;
@@ -17,15 +18,13 @@ class RecruitBoardTest {
     void createRecruitBoardWithDefaultStatus() {
         // given
         UUID centerId = UUID.randomUUID();
-        Long locationId = 1L;
-        RecruitBoard board = createRecruitBoard(centerId, locationId);
+        RecruitBoard board = createRecruitBoard(centerId);
 
         // when
         RecruitStatus recruitStatus = board.getRecruitStatus();
 
         // then
         assertThat(board.getCenterId()).isEqualTo(centerId);
-        assertThat(board.getLocationId()).isEqualTo(locationId);
         assertThat(recruitStatus).isEqualTo(RECRUITING);
     }
 
@@ -34,10 +33,11 @@ class RecruitBoardTest {
     void testCalculateVolunteerTime() {
         // given
         int hours = 3;
+        UUID centerId = UUID.randomUUID();
         LocalDateTime startDateTime = LocalDateTime.now();
         LocalDateTime endDateTime = startDateTime.plusHours(hours);
 
-        RecruitBoard board = createRecruitBoard(startDateTime, endDateTime);
+        RecruitBoard board = createRecruitBoard(centerId, startDateTime, endDateTime);
 
         // when
         LocalTime volunteerTime = board.getVolunteerHours();
@@ -46,27 +46,85 @@ class RecruitBoardTest {
         assertThat(volunteerTime).isEqualTo(LocalTime.of(hours, 0));
     }
 
-    private static RecruitBoard createRecruitBoard(UUID centerId, Long locationId) {
-        VolunteerInfo volunteerInfo = VolunteerInfo.builder()
-            .region("경기")
-            .recruitmentCount(1)
-            .volunteerStartDateTime(LocalDateTime.now())
-            .volunteerEndDateTime(LocalDateTime.now().plusHours(1))
-            .volunteerType(OTHER)
-            .admitted(true)
-            .build();
+    @DisplayName("봉사 모집글을 업데이트 할 수 있다")
+    @Test
+    void updateRecruitBoard() {
+        // given
+        UUID centerId = UUID.randomUUID();
+        RecruitBoard board = createRecruitBoard(centerId);
+        String imgUrl = "https://image.domain.com/updates";
 
-        return RecruitBoard.builder()
-            .centerId(centerId)
-            .locationId(locationId)
-            .title("봉사모집제목")
-            .content("봉사모집내용")
-            .imgUrl("https://image.domain.com/links")
-            .volunteerInfo(volunteerInfo)
-            .build();
+        RecruitBoardUpdateRequestDto dto = RecruitBoardUpdateRequestDto.builder()
+            .title("봉사 모집글 작성 수정")
+            .content("봉사 하실분을 모집합니다. 수정 <br>")
+            .recruitmentCount(10)
+            .volunteerStartDateTime(LocalDateTime.now())
+            .volunteerEndDateTime(LocalDateTime.now().plusHours(2))
+            .volunteerType(OTHER)
+            .admitted(true).build();
+
+        // when
+        board.updateWith(dto, imgUrl);
+
+        // then
+        assertThat(board.getTitle()).isEqualTo(dto.title());
+        assertThat(board.getContent()).isEqualTo(dto.content());
+        assertThat(board.getImgUrl()).isEqualTo(imgUrl);
     }
 
-    private static RecruitBoard createRecruitBoard(LocalDateTime startDateTime,
+    @DisplayName("봉사 활동 지역을 수정할 수 있다.")
+    @Test
+    void updateWithRegion() {
+        // given
+        UUID centerId = UUID.randomUUID();
+        RecruitBoard board = createRecruitBoard(centerId);
+        String updateRegion = "새로운지역";
+
+        // when
+        board.updateWith(updateRegion);
+
+        // then
+        VolunteerInfo volunteerInfo = board.getVolunteerInfo();
+        assertThat(volunteerInfo.getRegion()).isEqualTo(updateRegion);
+    }
+
+    @DisplayName("올바른 기관 식별 값이 주어지면 작성자인지 확인할 수 있다")
+    @Test
+    void isWriterWithCorrectCenterId() {
+        // given
+        UUID centerId = UUID.randomUUID();
+        RecruitBoard recruitBoard = createRecruitBoard(centerId);
+
+        // when
+        boolean isWriter = recruitBoard.isWriter(centerId);
+
+        // then
+        assertThat(isWriter).isTrue();
+    }
+
+    @DisplayName("잘못된 기관 식별 값이 주어지면 잘못된 작성자인 확인할 수있다.")
+    @Test
+    void isNotWriterWithWrongCenterId() {
+        UUID centerId = UUID.randomUUID();
+        UUID wrongId = UUID.randomUUID();
+        RecruitBoard recruitBoard = createRecruitBoard(centerId);
+
+        // when
+        boolean isNotWriter = recruitBoard.isNotWriter(wrongId);
+
+        // then
+        assertThat(isNotWriter).isTrue();
+    }
+
+
+    private static RecruitBoard createRecruitBoard(UUID centerId) {
+        LocalDateTime startDateTime = LocalDateTime.now();
+        LocalDateTime endDateTime = startDateTime.plusHours(1);
+
+        return createRecruitBoard(centerId, startDateTime, endDateTime);
+    }
+
+    private static RecruitBoard createRecruitBoard(UUID centerId, LocalDateTime startDateTime,
         LocalDateTime endDateTime) {
 
         VolunteerInfo volunteerInfo = VolunteerInfo.builder()
@@ -79,7 +137,7 @@ class RecruitBoardTest {
             .build();
 
         return RecruitBoard.builder()
-            .centerId(UUID.randomUUID())
+            .centerId(centerId)
             .locationId(1L)
             .title("봉사모집제목")
             .content("봉사모집내용")
--- src/test/java/com/somemore/recruitboard/domain/VolunteerInfoTest.java
+++ src/test/java/com/somemore/recruitboard/domain/VolunteerInfoTest.java
@@ -1,5 +1,7 @@
 package com.somemore.recruitboard.domain;
 
+import static com.somemore.recruitboard.domain.VolunteerType.ADMINISTRATIVE_SUPPORT;
+import static com.somemore.recruitboard.domain.VolunteerType.SAFETY_PREVENTION;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -43,7 +45,64 @@ class VolunteerInfoTest {
         assertThat(volunteerTime).isEqualTo(LocalTime.of(hours, 0));
     }
 
-    private static VolunteerInfo createVolunteerInfo(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+    @DisplayName("봉사 활동 정보를 업데이트 할 수 있다")
+    @Test
+    void updateVolunteerInfo() {
+        // given
+        VolunteerInfo volunteerInfo = createVolunteerInfo();
+
+        Integer count = 2;
+        VolunteerType volunteerType = SAFETY_PREVENTION;
+        LocalDateTime startDateTime = LocalDateTime.now();
+        LocalDateTime endDateTime = startDateTime.plusHours(2);
+        Boolean admitted = false;
+
+        // when
+        volunteerInfo.updateWith(count, volunteerType, startDateTime,
+            endDateTime, admitted);
+
+        // then
+        assertThat(volunteerInfo.getRecruitmentCount()).isEqualTo(count);
+        assertThat(volunteerInfo.getVolunteerType()).isEqualTo(volunteerType);
+        assertThat(volunteerInfo.getVolunteerStartDateTime().compareTo(startDateTime)).isZero();
+        assertThat(volunteerInfo.getVolunteerEndDateTime().compareTo(endDateTime)).isZero();
+        assertThat(volunteerInfo.getAdmitted()).isEqualTo(admitted);
+    }
+
+    @DisplayName("봉사활동 지역 정보를 업데이트할 수 있다")
+    @Test
+    void updateVolunteerInfoWithRegion() {
+        // given
+        VolunteerInfo volunteerInfo = createVolunteerInfo();
+        String updateRegion = "새로운지역";
+
+        // when
+        volunteerInfo.updateWith(updateRegion);
+
+        // then
+        assertThat(volunteerInfo.getRegion()).isEqualTo(updateRegion);
+    }
+
+    @DisplayName("봉사 종료 시간이 시작 시간과 같거나 빠르면, 봉사 모집글을 업데이트시 에러가 발생한다")
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1})
+    void updateRecruitBoardWithInValidVolunteerTime(long secondsOffset) {
+        // given
+        LocalDateTime startDateTime = LocalDateTime.now();
+        LocalDateTime endDateTime = startDateTime.plusSeconds(secondsOffset);
+
+        VolunteerInfo volunteerInfo = createVolunteerInfo();
+
+        // when & then
+        assertThatThrownBy(
+            () -> volunteerInfo.updateWith(3, ADMINISTRATIVE_SUPPORT, startDateTime, endDateTime,
+                false)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    private static VolunteerInfo createVolunteerInfo(LocalDateTime startDateTime,
+        LocalDateTime endDateTime) {
         return VolunteerInfo.builder()
             .region("경기")
             .recruitmentCount(1)
@@ -53,4 +112,9 @@ class VolunteerInfoTest {
             .admitted(true)
             .build();
     }
+
+    private static VolunteerInfo createVolunteerInfo() {
+        return createVolunteerInfo(LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+    }
+
 }
