b3bfdbbc91030b4b8c64bfcdd54aa06cedd1a287
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-28 16:54:56 +0900

    feat(Volunteer): 레포지토리 인터페이스, 구현체 추가, 리팩토링

--- src/main/java/com/somemore/volunteer/repository/VolunteerRepositoryImpl.java
+++ src/main/java/com/somemore/volunteer/repository/VolunteerRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.somemore.volunteer.repository;
 
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.somemore.volunteer.domain.QVolunteer;
 import com.somemore.volunteer.domain.Volunteer;
@@ -8,13 +10,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
 
 @RequiredArgsConstructor
 @Repository
-public class VolunteerRepositoryImpl implements VolunteerRepository{
+public class VolunteerRepositoryImpl implements VolunteerRepository {
 
     private final VolunteerJpaRepository volunteerJpaRepository;
     private final JPAQueryFactory queryFactory;
+
+    private final QVolunteer volunteer = QVolunteer.volunteer;
 
     @Override
     public Volunteer save(Volunteer volunteer) {
@@ -22,23 +27,54 @@ public class VolunteerRepositoryImpl implements VolunteerRepository{
     }
 
     @Override
-    public String findNicknameById(UUID id) {
-        QVolunteer volunteer = QVolunteer.volunteer;
-
-        return queryFactory
-                .select(volunteer.nickname)
-                .from(volunteer)
-                .where(volunteer.id.eq(id))
-                .fetchOne();
+    public Optional<Volunteer> findById(UUID id) {
+        return findOne(volunteer.id.eq(id));
     }
 
     @Override
     public Optional<Volunteer> findByOauthId(String oauthId) {
-        return volunteerJpaRepository.findByOauthId(oauthId);
+        return findOne(volunteer.oauthId.eq(oauthId));
+    }
+
+    @Override
+    public String findNicknameById(UUID id) {
+        return findDynamicField(id, volunteer.nickname)
+                .orElse(null);
     }
 
     @Override
     public void deleteAllInBatch() {
         volunteerJpaRepository.deleteAllInBatch();
+    }
+
+    private Optional<Volunteer> findOne(BooleanExpression condition) {
+
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(volunteer)
+                        .where(
+                                condition,
+                                isNotDeleted()
+                        )
+                        .fetchOne()
+        );
+    }
+
+    private <T> Optional<T> findDynamicField(UUID id, Path<T> field) {
+
+        return Optional.ofNullable(
+                queryFactory
+                        .select(field)
+                        .from(volunteer)
+                        .where(
+                                volunteer.id.eq(id),
+                                isNotDeleted()
+                        )
+                        .fetchOne()
+        );
+    }
+
+    private BooleanExpression isNotDeleted() {
+        return volunteer.deleted.isFalse();
     }
 }
