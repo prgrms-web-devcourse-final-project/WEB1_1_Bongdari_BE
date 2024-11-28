6977d5df2ea780daa8cb8eac49a8d6dada2b8f07
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-28 16:43:53 +0900

    refactor(Volunteer): Find UseCase/Service를 Query UseCase/Service 로 통합

--- src/main/java/com/somemore/volunteer/usecase/FindVolunteerIdUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.somemore.volunteer.usecase;
-
-import java.util.UUID;
-
-public interface FindVolunteerIdUseCase {
-    UUID findVolunteerIdByOAuthId(String oAuthId);
-    String getNicknameById(UUID id);
-}
