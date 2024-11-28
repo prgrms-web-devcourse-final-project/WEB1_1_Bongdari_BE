f7d34789b97cfabd348cdcec444f915bab87eb23
ayoung-dev <dkdudab@hanmail.net>
2024-11-28 17:26:33 +0900

    chore(community): 불필요한 import 제거

--- src/main/java/com/somemore/community/repository/comment/CommunityCommentRepositoryImpl.java
+++ src/main/java/com/somemore/community/repository/comment/CommunityCommentRepositoryImpl.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Repository
