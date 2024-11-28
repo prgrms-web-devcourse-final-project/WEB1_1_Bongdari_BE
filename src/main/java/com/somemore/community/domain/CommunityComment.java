4b666aa19434449492506b7d71b750a577dd3d5f
ayoung-dev <dkdudab@hanmail.net>
2024-11-28 11:28:03 +0900

    feat(community): communityComment Entity, Repository 추가

---
+++ src/main/java/com/somemore/community/domain/CommunityComment.java
@@ -0,0 +1,41 @@
+package com.somemore.community.domain;
+
+import com.somemore.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+import static lombok.AccessLevel.PROTECTED;
+
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+@Table(name = "Community_comment")
+public class CommunityComment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "writer_id", nullable = false, length = 16)
+    private UUID writerId;
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "parent_comment_id")
+    private Long parentCommentId;
+
+    @Builder
+    public CommunityComment(UUID writerId, String content, Long parentCommentId) {
+        this.writerId = writerId;
+        this.content = content;
+        this.parentCommentId = parentCommentId;
+    }
+}
