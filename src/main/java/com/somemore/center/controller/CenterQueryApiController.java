cc00129dcbfa5afd7b54eba5db90593cc411fcc1
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-11-29 01:05:13 +0900

    feat: 기관 프로필 조회 컨트롤러 구현
    - 기관 프로필 조회 엔드포인트를 위한 컨트롤러 구현
    - center의 PK값에 columnDefinition = "BINARY(16)" 옵션을 넣어 타입을 더 명시적으로 나타냄
    - 테스트 코드 작성및 검증완료

---
+++ src/main/java/com/somemore/center/controller/CenterQueryApiController.java
@@ -0,0 +1,28 @@
+package com.somemore.center.controller;
+
+import com.somemore.center.dto.response.CenterProfileResponseDto;
+import com.somemore.center.usecase.query.CenterQueryUseCase;
+import com.somemore.global.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/center")
+public class CenterQueryApiController {
+
+    private final CenterQueryUseCase centerQueryUseCase;
+
+    @GetMapping("/profile/{centerId}")
+    public ApiResponse<CenterProfileResponseDto> getCenterProfile(@PathVariable UUID centerId) {
+
+        CenterProfileResponseDto responseDto = centerQueryUseCase.getCenterProfileByCenterId(centerId);
+
+        return ApiResponse.ok(200, responseDto, "기관 프로필 조회 성공");
+    }
+}
