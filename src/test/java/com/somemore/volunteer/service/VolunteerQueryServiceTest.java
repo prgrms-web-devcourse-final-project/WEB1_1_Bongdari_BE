c737e850d57d125eb873b2e5c0054a76aedadd04
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-28 17:02:32 +0900

    refactor: 기존의 findService를 QueryService로 리팩토링

---
+++ src/test/java/com/somemore/volunteer/service/VolunteerQueryServiceTest.java
@@ -0,0 +1,79 @@
+package com.somemore.volunteer.service;
+
+import com.somemore.IntegrationTestSupport;
+import com.somemore.auth.oauth.OAuthProvider;
+import com.somemore.global.exception.BadRequestException;
+import com.somemore.volunteer.domain.Volunteer;
+import com.somemore.volunteer.repository.VolunteerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+import static com.somemore.global.exception.ExceptionMessage.NOT_EXISTS_VOLUNTEER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class VolunteerQueryServiceTest extends IntegrationTestSupport {
+
+    @Autowired
+    private VolunteerQueryService volunteerQueryService;
+
+    @Autowired
+    private VolunteerRepository volunteerRepository;
+
+    @AfterEach
+    void tearDown() {
+        volunteerRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("존재하는 OAuth ID로 봉사자 ID를 조회한다")
+    @Test
+    void findVolunteerId() {
+        // given
+        String oAuthId = "example-oauth-id";
+        Volunteer volunteer = Volunteer.createDefault(OAuthProvider.NAVER, oAuthId);
+
+        volunteerRepository.save(volunteer);
+
+        // when
+        UUID actualId = volunteerQueryService.getVolunteerIdByOAuthId(oAuthId);
+
+        // then
+        assertThat(actualId)
+                .isNotNull()
+                .isEqualTo(volunteer.getId());
+    }
+
+    @DisplayName("존재하지 않는 OAuth ID로 조회 시 예외를 던진다")
+    @Test
+    void throwExceptionWhenVolunteerNotFound() {
+        // given
+        String oAuthId = "non-existing-oauth-id";
+
+        // when
+        // then
+        assertThatThrownBy(() -> volunteerQueryService.getVolunteerIdByOAuthId(oAuthId))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage(NOT_EXISTS_VOLUNTEER.getMessage());
+    }
+
+    @DisplayName("봉사자의 id로 nickname을 조회한다.")
+    @Test
+    void getNicknameById() {
+
+        //given
+        String oAuthId = "example-oauth-id";
+        Volunteer volunteer = Volunteer.createDefault(OAuthProvider.NAVER, oAuthId);
+
+        volunteerRepository.save(volunteer);
+
+        //when
+        String nickname = volunteerQueryService.getNicknameById(volunteer.getId());
+
+        //then
+        assertThat(nickname).isEqualTo(volunteer.getNickname());
+    }
+}
