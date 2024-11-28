c246db5c3ded2b54f207c831496c2d102194b514
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-28 16:54:56 +0900

    feat(Volunteer): 레포지토리 인터페이스, 구현체 추가, 리팩토링

--- src/main/java/com/somemore/volunteer/repository/VolunteerJpaRepository.java
+++ src/main/java/com/somemore/volunteer/repository/VolunteerJpaRepository.java
@@ -4,7 +4,7 @@ import com.somemore.volunteer.domain.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface VolunteerJpaRepository extends JpaRepository<Volunteer, Long> {
-    Optional<Volunteer> findByOauthId(String oauthId);
 }
