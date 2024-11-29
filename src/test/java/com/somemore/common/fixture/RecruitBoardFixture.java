849b0d01d8872d7887f1fcfdb657e1db02396276
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-29 21:51:28 +0900

    test(RecruitBoardFixture): completed 상태의 RecruitBoard 생성 추가
    - 리플렉션을 이용해서 status를 수정.

--- src/test/java/com/somemore/common/fixture/RecruitBoardFixture.java
+++ src/test/java/com/somemore/common/fixture/RecruitBoardFixture.java
@@ -1,13 +1,16 @@
 package com.somemore.common.fixture;
 
-import static com.somemore.common.fixture.LocalDateTimeFixture.createStartDateTime;
-import static com.somemore.recruitboard.domain.VolunteerType.OTHER;
-
 import com.somemore.recruitboard.domain.RecruitBoard;
+import com.somemore.recruitboard.domain.RecruitStatus;
 import com.somemore.recruitboard.domain.RecruitmentInfo;
 import com.somemore.recruitboard.domain.VolunteerType;
+
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+import static com.somemore.common.fixture.LocalDateTimeFixture.createStartDateTime;
+import static com.somemore.recruitboard.domain.VolunteerType.OTHER;
 
 public class RecruitBoardFixture {
 
@@ -28,231 +31,266 @@ public class RecruitBoardFixture {
     public static RecruitBoard createRecruitBoard() {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(UUID.randomUUID())
-            .locationId(LOCATION_ID)
-            .title(TITLE)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(UUID.randomUUID())
+                .locationId(LOCATION_ID)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(String title) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(UUID.randomUUID())
-            .locationId(LOCATION_ID)
-            .title(title)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(UUID.randomUUID())
+                .locationId(LOCATION_ID)
+                .title(title)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(String title, UUID centerId, Long locationId) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(centerId)
-            .locationId(locationId)
-            .title(title)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(centerId)
+                .locationId(locationId)
+                .title(title)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(String title, UUID centerId) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(centerId)
-            .locationId(LOCATION_ID)
-            .title(title)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(centerId)
+                .locationId(LOCATION_ID)
+                .title(title)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(VolunteerType type, UUID centerId) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(type)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(type)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(centerId)
-            .locationId(LOCATION_ID)
-            .title(TITLE)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(centerId)
+                .locationId(LOCATION_ID)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(Boolean admitted, UUID centerId) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(admitted)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(admitted)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(centerId)
-            .locationId(LOCATION_ID)
-            .title(TITLE)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(centerId)
+                .locationId(LOCATION_ID)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(Long locationId) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(UUID.randomUUID())
-            .locationId(locationId)
-            .title(TITLE)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(UUID.randomUUID())
+                .locationId(locationId)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(UUID centerId) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(centerId)
-            .locationId(LOCATION_ID)
-            .title(TITLE)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(centerId)
+                .locationId(LOCATION_ID)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(UUID centerId, Long locationId) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(centerId)
-            .locationId(locationId)
-            .title(TITLE)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(centerId)
+                .locationId(locationId)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(String region, VolunteerType volunteerType) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(region)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(volunteerType)
-            .admitted(ADMITTED)
-            .build();
+                .region(region)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(volunteerType)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(UUID.randomUUID())
-            .locationId(LOCATION_ID)
-            .title(TITLE)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(UUID.randomUUID())
+                .locationId(LOCATION_ID)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
     }
 
     public static RecruitBoard createRecruitBoard(Long locationId, String title) {
 
         RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
-            .region(REGION)
-            .recruitmentCount(RECRUITMENT_COUNT)
-            .volunteerStartDateTime(START_DATE_TIME)
-            .volunteerEndDateTime(END_DATE_TIME)
-            .volunteerType(VOLUNTEER_TYPE)
-            .admitted(ADMITTED)
-            .build();
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
 
         return RecruitBoard.builder()
-            .centerId(UUID.randomUUID())
-            .locationId(locationId)
-            .title(title)
-            .content(CONTENT)
-            .imgUrl(IMG_URL)
-            .recruitmentInfo(recruitmentInfo)
-            .build();
+                .centerId(UUID.randomUUID())
+                .locationId(locationId)
+                .title(title)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
+    }
+
+    public static RecruitBoard createCompletedRecruitBoard() {
+
+        RecruitmentInfo recruitmentInfo = RecruitmentInfo.builder()
+                .region(REGION)
+                .recruitmentCount(RECRUITMENT_COUNT)
+                .volunteerStartDateTime(START_DATE_TIME)
+                .volunteerEndDateTime(END_DATE_TIME)
+                .volunteerType(VOLUNTEER_TYPE)
+                .admitted(ADMITTED)
+                .build();
+
+        RecruitBoard recruitBoard = RecruitBoard.builder()
+                .centerId(UUID.randomUUID())
+                .locationId(LOCATION_ID)
+                .title(TITLE)
+                .content(CONTENT)
+                .imgUrl(IMG_URL)
+                .recruitmentInfo(recruitmentInfo)
+                .build();
+
+        setRecruitStatusCompleted(recruitBoard);
+
+        return recruitBoard;
+    }
+
+    private static void setRecruitStatusCompleted(RecruitBoard recruitBoard) {
+        try {
+            Field recruitStatusField = RecruitBoard.class.getDeclaredField("recruitStatus");
+            recruitStatusField.setAccessible(true); // private 필드 접근 가능 설정
+            recruitStatusField.set(recruitBoard, RecruitStatus.COMPLETED); // 필드 값 설정
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException("리플렉션으로 recruitStatus를 설정하는 것에 실패했습니다", e);
+        }
     }
 }
