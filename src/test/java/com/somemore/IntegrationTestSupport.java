7137b8f035328bfe5acbf510f7f61c7d2f9e7941
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-30 00:20:20 +0900

    feat(IntegrationTestSupport): @AutoConfigureMockMvc 추가

--- src/test/java/com/somemore/IntegrationTestSupport.java
+++ src/test/java/com/somemore/IntegrationTestSupport.java
@@ -1,10 +1,12 @@
 package com.somemore;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 public abstract class IntegrationTestSupport {
 
 }
