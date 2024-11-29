82bf3f22ce30ce196969d8434606397c27b0435c
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-11-29 18:05:32 +0900

    feat: 관심기관 API 컨트롤러 구현
    - 관심 기관 등록과 취소 엔드포인트 컨트롤러 구현
    - ApiResponse 공통 응답 객체 메서드 추가(논의 예정)
    - 테스트 코드 작성및 검증완료

---
+++ src/main/java/com/somemore/interestcenter/controller/InterestCenterCommandApiController.java
@@ -0,0 +1,41 @@
+package com.somemore.interestcenter.controller;
+
+import com.somemore.global.common.response.ApiResponse;
+import com.somemore.interestcenter.dto.request.RegisterInterestCenterRequestDto;
+import com.somemore.interestcenter.dto.response.RegisterInterestCenterResponseDto;
+import com.somemore.interestcenter.usecase.CancelInterestCenterUseCase;
+import com.somemore.interestcenter.usecase.RegisterInterestCenterUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/interest-center")
+@Tag(name = "Interest Center Command API", description = "관심 기관의 등록과 취소 API를 제공합니다")
+public class InterestCenterCommandApiController {
+
+    private final RegisterInterestCenterUseCase registerInterestCenterUseCase;
+    private final CancelInterestCenterUseCase cancelInterestCenterUseCase;
+
+    @Operation(summary = "관심기관 등록 API")
+    @PostMapping()
+    public ApiResponse<RegisterInterestCenterResponseDto> registerInterestCenter(@RequestBody RegisterInterestCenterRequestDto requestDto) {
+
+        RegisterInterestCenterResponseDto responseDto = registerInterestCenterUseCase.registerInterestCenter(requestDto);
+
+        return ApiResponse.ok(200, responseDto, "관심 기관 등록 성공");
+    }
+
+    @Operation(summary = "관심기관 취소 API")
+    @DeleteMapping("/{interest-center-id}")
+    public ApiResponse<String> deleteInterestCenter(@PathVariable("interest-center-id") Long interestCenterId) {
+
+        cancelInterestCenterUseCase.cancelInterestCenter(interestCenterId);
+
+        return ApiResponse.okWithoutData("관심 기관 취소 성공");
+    }
+
+
+}
