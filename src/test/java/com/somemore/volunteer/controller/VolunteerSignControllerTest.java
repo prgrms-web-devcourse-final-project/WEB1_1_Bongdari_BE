a7c176c46f19bbfa09625beb90698846df039dfc
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-30 00:20:27 +0900

    test(VolunteerSignController): ControllerTestSupport 상속받도록 변경

--- src/test/java/com/somemore/volunteer/controller/VolunteerSignControllerTest.java
+++ src/test/java/com/somemore/volunteer/controller/VolunteerSignControllerTest.java
@@ -1,11 +1,9 @@
 package com.somemore.volunteer.controller;
 
-import com.somemore.IntegrationTestSupport;
+import com.somemore.ControllerTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,9 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class VolunteerSignControllerTest extends IntegrationTestSupport {
+class VolunteerSignControllerTest extends ControllerTestSupport {
 
     @Autowired
     private MockMvc mockMvc;
