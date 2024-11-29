8609f23100d9f93920ffc0d39029c671eb2432de
leebs0521 <leebs0521@gmail.com>
2024-11-29 14:19:19 +0900

    refactor(location): Location 조회 수정 기능 리팩토링

--- src/main/java/com/somemore/location/service/query/LocationQueryService.java
+++ src/main/java/com/somemore/location/service/query/LocationQueryService.java
@@ -1,9 +1,11 @@
 package com.somemore.location.service.query;
 
+import static com.somemore.global.exception.ExceptionMessage.NOT_EXISTS_LOCATION;
+
+import com.somemore.global.exception.BadRequestException;
 import com.somemore.location.domain.Location;
 import com.somemore.location.repository.LocationRepository;
 import com.somemore.location.usecase.query.LocationQueryUseCase;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +18,14 @@ public class LocationQueryService implements LocationQueryUseCase {
     private final LocationRepository locationRepository;
 
     @Override
-    public Optional<Location> findById(Long id) {
-        return locationRepository.findById(id);
+    public Location getById(Long id) {
+        return getLocation(id);
+    }
+
+    private Location getLocation(Long id) {
+        return locationRepository.findById(id).orElseThrow(
+            () -> new BadRequestException(NOT_EXISTS_LOCATION.getMessage())
+        );
     }
 
 }
