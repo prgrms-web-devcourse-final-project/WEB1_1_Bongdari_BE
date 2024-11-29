2974fc811fc96d0d86d54f6fe99c5f91647aa5e8
leebs0521 <leebs0521@gmail.com>
2024-11-29 12:08:55 +0900

    test: TestControllerSupport 으로 변경

--- src/test/java/com/somemore/recruitboard/controller/RecruitBoardQueryControllerTest.java
+++ src/test/java/com/somemore/recruitboard/controller/RecruitBoardQueryControllerTest.java
@@ -1,16 +1,16 @@
 package com.somemore.recruitboard.controller;
 
 import static com.somemore.recruitboard.domain.VolunteerType.ADMINISTRATIVE_SUPPORT;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.somemore.IntegrationTestSupport;
+import com.somemore.ControllerTestSupport;
 import com.somemore.recruitboard.dto.condition.RecruitBoardNearByCondition;
 import com.somemore.recruitboard.dto.condition.RecruitBoardSearchCondition;
 import com.somemore.recruitboard.dto.response.RecruitBoardDetailResponseDto;
@@ -23,18 +23,13 @@ import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class RecruitBoardQueryControllerTest extends IntegrationTestSupport {
+class RecruitBoardQueryControllerTest extends ControllerTestSupport {
 
     @Autowired
     private MockMvc mockMvc;
@@ -140,19 +135,21 @@ class RecruitBoardQueryControllerTest extends IntegrationTestSupport {
         UUID centerId = UUID.randomUUID();
         Page<RecruitBoardResponseDto> page = new PageImpl<>(Collections.emptyList());
 
-        given(recruitBoardQueryUseCase.getRecruitBoardsByCenterId(centerId,
+        given(recruitBoardQueryUseCase.getRecruitBoardsByCenterId(eq(centerId),
             any(RecruitBoardSearchCondition.class)))
             .willReturn(page);
 
         // when
         // then
         mockMvc.perform(get("/api/recruit-boards/center/{centerId}", centerId)
+                .param("keyword", "volunteer")
+                .param("type", ADMINISTRATIVE_SUPPORT.name())
                 .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.data").exists())
             .andExpect(jsonPath("$.message").value("기관 봉사 활동 모집글 조회 성공"));
 
-        verify(recruitBoardQueryUseCase, times(1)).getRecruitBoardsByCenterId(centerId,
+        verify(recruitBoardQueryUseCase, times(1)).getRecruitBoardsByCenterId(eq(centerId),
             any(RecruitBoardSearchCondition.class));
     }
 }
