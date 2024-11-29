9de3df577bb4b0c0027f779b455999716a005906
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-29 21:37:44 +0900

    feat(VolunteerDetailRepository): 사용하지 않는 메서드 삭제

--- src/main/java/com/somemore/volunteer/repository/VolunteerDetailRepository.java
+++ src/main/java/com/somemore/volunteer/repository/VolunteerDetailRepository.java
@@ -10,5 +10,4 @@ import java.util.UUID;
 public interface VolunteerDetailRepository {
     VolunteerDetail save(VolunteerDetail volunteerDetail);
     Optional<VolunteerDetail> findByVolunteerId(UUID volunteerId);
-    void deleteAllInBatch();
 }
