a2683a947017d005e1b58cd51e61daeec4e8cf2b
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-29 21:33:52 +0900

    feat(VolunteerApply): 엔티티와 ApplyStatus ENUM 추가

---
+++ src/main/java/com/somemore/volunteerApply/domain/VolunteerApply.java
@@ -0,0 +1,44 @@
+package com.somemore.volunteerApply.domain;
+
+import com.somemore.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "volunteer_apply")
+public class VolunteerApply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "volunteer_id", nullable = false, columnDefinition = "BINARY(16)")
+    private UUID volunteerId;
+
+    @Column(name = "recruit_board_id", nullable = false)
+    private Long recruitBoardId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private ApplyStatus status;
+
+    @Column(name = "attended", nullable = false)
+    private Boolean attended;
+
+    @Builder
+    public VolunteerApply(UUID volunteerId, Long recruitBoardId, ApplyStatus status, Boolean attended) {
+        this.volunteerId = volunteerId;
+        this.recruitBoardId = recruitBoardId;
+        this.status = status != null ? status : ApplyStatus.WAITING;
+        this.attended = attended != null ? attended : false;
+    }
+}
+
+// TODO 상태 업데이트 메서드들을 만들고 빌더에서 status를 변경 불가하도록
