ed06d86b02282584fc316251af6c495a8da03b09
ayoung-dev <dkdudab@hanmail.net>
2024-11-29 12:20:49 +0900

    test(community): communityComment 수정 Usecase 및 Service 생성

---
+++ src/main/java/com/somemore/community/usecase/comment/UpdateCommunityCommentUseCase.java
@@ -0,0 +1,12 @@
+package com.somemore.community.usecase.comment;
+
+import com.somemore.community.dto.request.CommunityCommentUpdateRequestDto;
+
+import java.util.UUID;
+
+public interface UpdateCommunityCommentUseCase {
+    void updateCommunityComment(
+            CommunityCommentUpdateRequestDto requestDto,
+            Long communityCommentId,
+            UUID writerId);
+}
