70723b64be7f75adde39bb545a1fb77763d374b9
leebs0521 <leebs0521@gmail.com>
2024-11-30 17:45:26 +0900

    feat(center): 이름 기반 조회 기능 추가

--- src/main/java/com/somemore/center/repository/CenterJpaRepository.java
+++ src/main/java/com/somemore/center/repository/CenterJpaRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 public interface CenterJpaRepository extends JpaRepository<Center, Long> {
     boolean existsById(UUID id);
     Optional<Center> findCenterById(UUID id);
+    Optional<Center> findByName(String name);
 }
