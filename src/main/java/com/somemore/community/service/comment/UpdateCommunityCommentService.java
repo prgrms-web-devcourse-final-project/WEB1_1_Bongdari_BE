e514f406ef4281a1224d7769b13cb3631f318768
ayoung-dev <dkdudab@hanmail.net>
2024-11-30 23:41:55 +0900

    feat(community): communityComment 수정 시 게시글 존재 검증 메서드 추가

--- src/main/java/com/somemore/community/service/comment/UpdateCommunityCommentService.java
+++ src/main/java/com/somemore/community/service/comment/UpdateCommunityCommentService.java
@@ -2,6 +2,7 @@ package com.somemore.community.service.comment;
 
 import com.somemore.community.domain.CommunityComment;
 import com.somemore.community.dto.request.CommunityCommentUpdateRequestDto;
+import com.somemore.community.repository.board.CommunityBoardRepository;
 import com.somemore.community.repository.comment.CommunityCommentRepository;
 import com.somemore.community.usecase.comment.UpdateCommunityCommentUseCase;
 import com.somemore.global.exception.BadRequestException;
@@ -19,12 +20,17 @@ import static com.somemore.global.exception.ExceptionMessage.*;
 public class UpdateCommunityCommentService implements UpdateCommunityCommentUseCase {
 
     private final CommunityCommentRepository communityCommentRepository;
+    private final CommunityBoardRepository communityBoardRepository;
 
     @Override
     public void updateCommunityComment(CommunityCommentUpdateRequestDto requestDto, Long communityCommentId, UUID writerId) {
 
         CommunityComment communityComment = getCommunityCommentById(communityCommentId);
+
+        validateCommunityBoardExists(communityComment.getCommunityBoardId());
+
         validateWriter(communityComment, writerId);
+
         communityComment.updateWith(requestDto);
 
         communityCommentRepository.save(communityComment);
@@ -35,11 +41,17 @@ public class UpdateCommunityCommentService implements UpdateCommunityCommentUseC
                 .orElseThrow(() -> new BadRequestException(NOT_EXISTS_COMMUNITY_COMMENT.getMessage()));
     }
 
+    private void validateCommunityBoardExists(Long communityBoardId) {
+        if (communityBoardRepository.doesNotExistById(communityBoardId)) {
+            throw new BadRequestException(NOT_EXISTS_COMMUNITY_BOARD.getMessage());
+        }
+    }
+
     private void validateWriter(CommunityComment communityComment, UUID writerId) {
         if (communityComment.isWriter(writerId)) {
             return;
         }
-
+        
         throw new BadRequestException(UNAUTHORIZED_COMMUNITY_COMMENT.getMessage());
     }
 }
--- src/test/java/com/somemore/community/service/comment/UpdateCommunityCommentServiceTest.java
+++ src/test/java/com/somemore/community/service/comment/UpdateCommunityCommentServiceTest.java
@@ -1,16 +1,19 @@
 package com.somemore.community.service.comment;
 
+import static com.somemore.global.exception.ExceptionMessage.NOT_EXISTS_COMMUNITY_BOARD;
+import static com.somemore.global.exception.ExceptionMessage.UNAUTHORIZED_COMMUNITY_COMMENT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import com.somemore.IntegrationTestSupport;
+import com.somemore.community.domain.CommunityBoard;
 import com.somemore.community.domain.CommunityComment;
+import com.somemore.community.dto.request.CommunityBoardCreateRequestDto;
 import com.somemore.community.dto.request.CommunityCommentCreateRequestDto;
 import com.somemore.community.dto.request.CommunityCommentUpdateRequestDto;
+import com.somemore.community.repository.board.CommunityBoardRepository;
 import com.somemore.community.repository.comment.CommunityCommentRepository;
-import com.somemore.community.usecase.comment.CreateCommunityCommentUseCase;
 import com.somemore.global.exception.BadRequestException;
-import com.somemore.global.exception.ExceptionMessage;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,23 +29,38 @@ class UpdateCommunityCommentServiceTest extends IntegrationTestSupport {
     @Autowired
     private UpdateCommunityCommentService updateCommunityCommentService;
     @Autowired
-    private CreateCommunityCommentUseCase createCommunityCommentUseCase;
-    @Autowired
     private CommunityCommentRepository communityCommentRepository;
+    @Autowired
+    private CommunityBoardRepository communityBoardRepository;
 
     private UUID writerId;
     private Long commentId;
+    private CommunityCommentUpdateRequestDto updateRequestDto;
 
     @BeforeEach
     void setUp() {
-        CommunityCommentCreateRequestDto dto = CommunityCommentCreateRequestDto.builder()
-                .content("커뮤니티 댓글 테스트 내용")
-                .parentCommentId(null)
+        CommunityBoardCreateRequestDto boardDto = CommunityBoardCreateRequestDto.builder()
+                .title("커뮤니티 테스트 제목")
+                .content("커뮤니티 테스트 내용")
                 .build();
 
         writerId = UUID.randomUUID();
 
-        commentId = createCommunityCommentUseCase.createCommunityComment(dto, writerId);
+        CommunityBoard communityBoard = communityBoardRepository.save(boardDto.toEntity(writerId, "https://test.image/123"));
+
+        CommunityCommentCreateRequestDto commentDto = CommunityCommentCreateRequestDto.builder()
+                .communityBoardId(communityBoard.getId())
+                .content("커뮤니티 댓글 테스트 내용")
+                .parentCommentId(null)
+                .build();
+
+        CommunityComment communityComment = communityCommentRepository.save(commentDto.toEntity(writerId));
+
+        commentId = communityComment.getId();
+
+        updateRequestDto = CommunityCommentUpdateRequestDto.builder()
+                .content("수정한 커뮤니티 댓글 내용")
+                .build();
     }
 
     @AfterEach
@@ -53,13 +71,10 @@ class UpdateCommunityCommentServiceTest extends IntegrationTestSupport {
     @DisplayName("댓글을 수정한다.")
     @Test
     void updateCommunityComment() {
-        //given
-        CommunityCommentUpdateRequestDto dto = CommunityCommentUpdateRequestDto.builder()
-                .content("수정한 커뮤니티 댓글 내용")
-                .build();
 
+        //given
         //when
-        updateCommunityCommentService.updateCommunityComment(dto, commentId, writerId);
+        updateCommunityCommentService.updateCommunityComment(updateRequestDto, commentId, writerId);
 
         //then
         Optional<CommunityComment> communityComment = communityCommentRepository.findById(commentId);
@@ -71,17 +86,29 @@ class UpdateCommunityCommentServiceTest extends IntegrationTestSupport {
     @DisplayName("작성자가 아닌 id로 댓글을 수정하고자 할 때 예외를 던진다.")
     @Test
     void updateCommunityCommentWithNotWriterId() {
-        //given
-        CommunityCommentUpdateRequestDto dto = CommunityCommentUpdateRequestDto.builder()
-                .content("수정한 커뮤니티 댓글 내용")
-                .build();
 
+        //given
         //when
-        ThrowableAssert.ThrowingCallable callable = () -> updateCommunityCommentService.updateCommunityComment(dto, commentId, UUID.randomUUID());
+        ThrowableAssert.ThrowingCallable callable = () -> updateCommunityCommentService.updateCommunityComment(updateRequestDto, commentId, UUID.randomUUID());
 
         //then
         assertThatExceptionOfType(BadRequestException.class)
                 .isThrownBy(callable)
-                .withMessage(ExceptionMessage.UNAUTHORIZED_COMMUNITY_COMMENT.getMessage());
+                .withMessage(UNAUTHORIZED_COMMUNITY_COMMENT.getMessage());
+    }
+
+    @DisplayName("삭제된 게시글의 댓글을 수정하고자 할 때 예외를 던진다.")
+    @Test
+    void updateCommunityCommentWithDeletedBoardId() {
+
+        //given
+        communityBoardRepository.deleteAllInBatch();
+        //when
+        ThrowableAssert.ThrowingCallable callable = () -> updateCommunityCommentService.updateCommunityComment(updateRequestDto, commentId, writerId);
+
+        //then
+        assertThatExceptionOfType(BadRequestException.class)
+                .isThrownBy(callable)
+                .withMessage(NOT_EXISTS_COMMUNITY_BOARD.getMessage());
     }
 }
