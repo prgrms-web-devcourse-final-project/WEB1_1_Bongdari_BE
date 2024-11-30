c1370be22e8b6adfc0b0185263a0b1ac190a1a08
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-30 23:42:20 +0900

    feat(VolunteerQueryController): @Secured 적용, Mapping 리팩토링

--- src/main/java/com/somemore/volunteer/controller/VolunteerQueryController.java
+++ src/main/java/com/somemore/volunteer/controller/VolunteerQueryController.java
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -17,13 +19,15 @@ import java.util.UUID;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@Tag(name = "Find Volunteer", description = "Find Volunteer")
+@RequestMapping("/api/profile")
+@Tag(name = "GET Volunteer", description = "봉사자 조회")
 public class VolunteerQueryController {
 
     private final VolunteerQueryUseCase volunteerQueryUseCase;
 
-    @GetMapping("/profile/me")
     @Operation(summary = "본인 상세 프로필 조회", description = "현재 로그인된 사용자의 상세 프로필을 조회합니다.")
+    @Secured("ROLE_VOLUNTEER")
+    @GetMapping("/me")
     public ApiResponse<VolunteerResponseDto> getMyProfile(
             @AuthenticationPrincipal String volunteerId) {
 
@@ -33,7 +37,7 @@ public class VolunteerQueryController {
                 "프로필 조회 성공");
     }
 
-    @GetMapping("/profile/{volunteerId}")
+    @GetMapping("/{volunteerId}")
     @Operation(summary = "타인 프로필 조회", description = "특정 봉사자의 프로필을 조회합니다. 상세 정보는 포함되지 않습니다.")
     public ApiResponse<VolunteerResponseDto> getVolunteerProfile(
             @PathVariable UUID volunteerId) {
@@ -45,7 +49,8 @@ public class VolunteerQueryController {
         );
     }
 
-    @GetMapping("/profile/{volunteerId}/detailed")
+    @GetMapping("/{volunteerId}/detailed")
+    @Secured("ROLE_CENTER")
     @Operation(summary = "타인 상세 프로필 조회", description = "기관이 작성한 모집 글에 지원한 봉사자의 상세 프로필을 조회합니다.")
     public ApiResponse<VolunteerResponseDto> getVolunteerDetailedProfile(
             @PathVariable UUID volunteerId,
