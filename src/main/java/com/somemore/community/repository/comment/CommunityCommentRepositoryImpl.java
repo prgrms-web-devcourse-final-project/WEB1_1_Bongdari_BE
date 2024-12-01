fe89e8641fe3d58dc68f14d16272ae98ae010471
ayoung-dev <dkdudab@hanmail.net>
2024-12-02 02:19:22 +0900

    feat(community): 게시글 ID로 댓글 조회 메서드 추가

--- src/main/java/com/somemore/community/repository/comment/CommunityCommentRepositoryImpl.java
+++ src/main/java/com/somemore/community/repository/comment/CommunityCommentRepositoryImpl.java
@@ -1,11 +1,15 @@
 package com.somemore.community.repository.comment;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.somemore.community.domain.CommunityComment;
 import com.somemore.community.domain.QCommunityComment;
+import com.somemore.community.repository.mapper.CommunityCommentView;
+import com.somemore.volunteer.domain.QVolunteer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -29,6 +33,21 @@ public class CommunityCommentRepositoryImpl implements CommunityCommentRepositor
                 .where(communityComment.id.eq(id)
                         .and(communityComment.deleted.eq(false)))
                 .fetchOne());
+    }
+
+    public List<CommunityCommentView> findCommentsByBoardId(Long boardId) {
+        QCommunityComment communityComment = QCommunityComment.communityComment;
+        QVolunteer volunteer = QVolunteer.volunteer;
+
+        return queryFactory
+                .select(Projections.constructor(CommunityCommentView.class,
+                        communityComment,
+                        volunteer.nickname))
+                .from(communityComment)
+                .join(volunteer).on(communityComment.writerId.eq(volunteer.id))
+                .where(communityComment.communityBoardId.eq(boardId))
+                .orderBy(communityComment.parentCommentId.asc().nullsFirst(), communityComment.createdAt.asc())
+                .fetch();
     }
 
     @Override
