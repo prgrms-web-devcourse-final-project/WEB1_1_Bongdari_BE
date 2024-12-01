cdc54c322936329b72bc3c5f00a915088075e85b
leebs0521 <leebs0521@gmail.com>
2024-12-01 13:14:47 +0900

    feat(volunteer-apply): 기본 엔티티 레포지토리 추가

---
+++ src/main/java/com/somemore/volunteerapply/repository/VolunteerApplyRepository.java
@@ -0,0 +1,18 @@
+package com.somemore.volunteerapply.repository;
+
+import com.somemore.volunteerapply.domain.VolunteerApply;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface VolunteerApplyRepository {
+
+    VolunteerApply save(VolunteerApply volunteerApply);
+    Optional<VolunteerApply> findById(Long id);
+    List<UUID> findVolunteerIdsByRecruitIds(List<Long> recruitIds);
+    Page<VolunteerApply> findAllByRecruitId(Long recruitId, Pageable pageable);
+    Optional<VolunteerApply> findByRecruitIdAndVolunteerId(Long recruitId, UUID volunteerId);
+
+}
