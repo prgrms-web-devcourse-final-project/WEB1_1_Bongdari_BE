76540611eadc546f1ce035a93c22761184a8319d
leebs0521 <leebs0521@gmail.com>
2024-12-01 13:15:55 +0900

    feat(volunteer-apply): 리뷰 생성에 필요한 조회 기능 추가

--- src/main/java/com/somemore/volunteerapply/usecase/VolunteerApplyQueryUseCase.java
+++ src/main/java/com/somemore/volunteerapply/usecase/VolunteerApplyQueryUseCase.java
@@ -1,9 +1,10 @@
 package com.somemore.volunteerapply.usecase;
 
+import com.somemore.volunteerapply.domain.VolunteerApply;
 import java.util.List;
 import java.util.UUID;
 
 public interface VolunteerApplyQueryUseCase {
-
     List<UUID> getVolunteerIdsByRecruitIds(List<Long> recruitIds);
+    VolunteerApply getByRecruitIdAndVolunteerId(Long recruitId, UUID volunteerId);
 }
