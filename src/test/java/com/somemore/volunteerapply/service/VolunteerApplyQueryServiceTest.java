d8106ff8e05002d606c50b9262cb81ed5d6a4e7f
leebs0521 <leebs0521@gmail.com>
2024-12-01 13:16:07 +0900

    test(volunteer-apply): 리뷰 생성에 필요한 조회 기능 테스트

--- src/test/java/com/somemore/volunteerapply/service/VolunteerApplyQueryServiceTest.java
+++ src/test/java/com/somemore/volunteerapply/service/VolunteerApplyQueryServiceTest.java
@@ -1,18 +1,17 @@
 package com.somemore.volunteerapply.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.somemore.IntegrationTestSupport;
 import com.somemore.volunteerapply.domain.ApplyStatus;
 import com.somemore.volunteerapply.domain.VolunteerApply;
 import com.somemore.volunteerapply.repository.VolunteerApplyRepository;
+import java.util.List;
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 class VolunteerApplyQueryServiceTest extends IntegrationTestSupport {
@@ -39,7 +38,8 @@ class VolunteerApplyQueryServiceTest extends IntegrationTestSupport {
         volunteerApplyRepository.save(apply2);
 
         // When
-        List<UUID> volunteerIds = volunteerApplyQueryService.getVolunteerIdsByRecruitIds(List.of(recruitId1, recruitId2));
+        List<UUID> volunteerIds = volunteerApplyQueryService.getVolunteerIdsByRecruitIds(
+                List.of(recruitId1, recruitId2));
 
         // Then
         assertThat(volunteerIds)
@@ -54,5 +54,29 @@ class VolunteerApplyQueryServiceTest extends IntegrationTestSupport {
                 .status(ApplyStatus.WAITING)
                 .attended(false)
                 .build();
+    }
+
+    @DisplayName("모집글 아이디와 봉사자 아이디로 조회할 수 있다")
+    @Test
+    void getByRecruitIdAndVolunteerId() {
+        // given
+        Long recruitId = 1234L;
+        UUID volunteerId = UUID.randomUUID();
+
+        VolunteerApply newApply = VolunteerApply.builder()
+                .volunteerId(volunteerId)
+                .recruitBoardId(recruitId)
+                .status(ApplyStatus.APPROVED)
+                .attended(false)
+                .build();
+        volunteerApplyRepository.save(newApply);
+
+        // when
+        VolunteerApply apply = volunteerApplyQueryService.getByRecruitIdAndVolunteerId(
+                recruitId, volunteerId);
+
+        // then
+        assertThat(apply.getRecruitBoardId()).isEqualTo(recruitId);
+        assertThat(apply.getVolunteerId()).isEqualTo(volunteerId);
     }
 }
