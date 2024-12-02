a2d0845de332e70d55ded7fa72b791f35140e517
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-12-02 11:22:31 +0900

    chore: 코드리뷰 사항 반영
    - 응답 객체에 swagger 스키마 적용
    - 개행 추

--- src/main/java/com/somemore/center/dto/response/PreferItemCreateResponseDto.java
+++ src/main/java/com/somemore/center/dto/response/PreferItemCreateResponseDto.java
@@ -3,6 +3,7 @@ package com.somemore.center.dto.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.somemore.center.domain.PreferItem;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.UUID;
@@ -10,8 +11,11 @@ import java.util.UUID;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Builder
 public record PreferItemCreateResponseDto(
+        @Schema(description = "선호물품의 ID", example = "111")
         Long id,
+        @Schema(description = "기관의 ID", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID centerId,
+        @Schema(description = "선호물품 이름", example = "어린이 도서")
         String itemName
 ) {
     public static PreferItemCreateResponseDto from(PreferItem preferItem) {
