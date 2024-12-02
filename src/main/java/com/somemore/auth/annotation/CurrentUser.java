50605103068a98220a64d69145787453813f1905
leebs0521 <leebs0521@gmail.com>
2024-12-03 00:49:58 +0900

    feat(auth): 인증된 유저 ID를 UUID 형태로 받도록 어노테이션 생성

---
+++ src/main/java/com/somemore/auth/annotation/CurrentUser.java
@@ -0,0 +1,14 @@
+package com.somemore.auth.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null :T(java.util.UUID).fromString(#this)")
+public @interface CurrentUser {
+
+}
