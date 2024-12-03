552a1f6218fb02f553c43ec382bf5be014f054ac
leebs0521 <leebs0521@gmail.com>
2024-12-03 17:53:50 +0900

    feat(volunteer-apply): 봉사 활동 지원 승인 API

---
+++ src/main/java/com/somemore/volunteerapply/controller/CenterVolunteerApplyCommandApiController.java
@@ -0,0 +1,36 @@
+package com.somemore.volunteerapply.controller;
+
+import com.somemore.auth.annotation.CurrentUser;
+import com.somemore.global.common.response.ApiResponse;
+import com.somemore.volunteerapply.usecase.ApproveVolunteerApplyUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Center Volunteer Apply Command API", description = "봉사 활동 지원 승인, 거절, 정산 API")
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class CenterVolunteerApplyCommandApiController {
+
+    private final ApproveVolunteerApplyUseCase approveVolunteerApplyUseCase;
+
+    @Secured("ROLE_CENTER")
+    @Operation(summary = "봉사 활동 지원 승인", description = "봉사 활동 지원을 승인합니다.")
+    @PatchMapping("/volunteer-apply/{id}/approve")
+    public ApiResponse<String> approve(
+            @CurrentUser UUID centerId,
+            @PathVariable Long id
+    ) {
+
+        approveVolunteerApplyUseCase.approve(id, centerId);
+        return ApiResponse.ok("봉사 활동 지원 승인 성공");
+    }
+
+}
