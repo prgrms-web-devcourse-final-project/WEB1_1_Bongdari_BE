13017c06c9959ec383963e94d341fe1c55a10cc9
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-03 20:23:30 +0900

    feat(Notification): BaseEntity 상속 취소, 빌더 추가, NotificationType 추가

--- src/main/java/com/somemore/notification/domain/Notification.java
+++ src/main/java/com/somemore/notification/domain/Notification.java
@@ -1,18 +1,20 @@
-package com.somemore.notification;
+package com.somemore.notification.domain;
 
-import com.somemore.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "notification")
-public class Notification extends BaseEntity {
+public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -24,14 +26,34 @@ public class Notification extends BaseEntity {
     @Column(name = "title", nullable = false)
     private String title;
 
+    @Column(name = "type", nullable = false)
+    private NotificationType type;
+
     @Column(name = "related_id", nullable = false)
     private Long relatedId;
 
     @Column(name = "read", nullable = false)
     private boolean read;
 
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
     void markAsRead() {
         this.read = true;
-        markAsDeleted();
+    }
+
+    @Builder
+    public Notification(
+            UUID receiverId,
+            String title,
+            NotificationType type,
+            Long relatedId
+    ) {
+        this.receiverId = receiverId;
+        this.title = title;
+        this.type = type;
+        this.relatedId = relatedId;
+        this.read = false;
     }
 }
