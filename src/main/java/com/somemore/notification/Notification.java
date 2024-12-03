0b4d26be1d48f8de782b5818ecc7f61d97dcc78b
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-03 12:32:07 +0900

    feat(Notification): 알림 도메인 추가

---
+++ src/main/java/com/somemore/notification/Notification.java
@@ -0,0 +1,32 @@
+package com.somemore.notification;
+
+import com.somemore.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "notification")
+public class Notification extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "receiver_id", nullable = false, length = 16)
+    private UUID receiverId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "related_id", nullable = false)
+    private Long relatedId;
+
+    @Column(name = "read", nullable = false)
+    private boolean read;
+}
