7e872f91a20b6e07ed644e663dfd0cbecaa927e8
leebs0521 <leebs0521@gmail.com>
2024-12-03 11:01:36 +0900

    feat(center-login): 기관 로그인 컨트롤러

--- src/main/java/com/somemore/center/controller/CenterSignController.java
+++ src/main/java/com/somemore/center/controller/CenterSignController.java
@@ -9,8 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,11 +22,15 @@ public class CenterSignController {
 
     private final SignOutUseCase signOutUseCase;
 
+    /*
+     * 기관 로그인 엔드포인트
+     * 실제 로그인 절차는 필터에서 처리됩니다.
+     * 이 엔드포인트는 로그인 요청을 받아 필터에 의한 인증 절차를 수행합니다.
+     */
     @PostMapping("/sign-in")
     public ApiResponse<String> signIn(
-            @RequestBody CenterSignRequestDto requestDto
+            @RequestParam CenterSignRequestDto requestDto
     ) {
-
         return ApiResponse.ok("로그인되었습니다.");
     }
 
