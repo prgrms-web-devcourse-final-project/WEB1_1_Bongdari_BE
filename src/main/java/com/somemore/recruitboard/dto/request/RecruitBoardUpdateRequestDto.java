2120ccbfd0d353ea1dcac772d51bfc018e059f3c
Beom-Su Lee <leebs0521@gmail.com>
2024-12-03 11:05:50 +0900

    refactor: 인증 유저 어노테이션 및 스웨거 관련 수정 (#123)
    * feat(auth): 인증된 유저 ID를 UUID 형태로 받도록 어노테이션 생성
    
    * feat(auth): 어노테이션 생성에 따른 컨트롤러 코드 수정
    
    * fix(swagger): 스웨거 API 문서 중 날짜의 경우 예제와 다른 값 출력 해결
    
    * test(auth): Auth Long -> UUID 에 따른 테스트 어노테이션 리졸버 수정
    
    * feat(common): 공통 응답 객체 스웨거 적용
    
    * feat(center-login): 기관 로그인 스웨거 명세 추가
    
    * chore: 불필요한 import 제거
    
    * feat(center-login): 기관 로그인 컨트롤러

--- src/main/java/com/somemore/recruitboard/dto/request/RecruitBoardUpdateRequestDto.java
+++ src/main/java/com/somemore/recruitboard/dto/request/RecruitBoardUpdateRequestDto.java
@@ -12,27 +12,27 @@ import lombok.Builder;
 @JsonNaming(SnakeCaseStrategy.class)
 @Builder
 public record RecruitBoardUpdateRequestDto(
-    @Schema(description = "봉사 모집글 제목", example = "서울 청계천 환경 미화 봉사 모집")
-    @NotBlank(message = "모집글 제목은 필수 값입니다.")
-    String title,
-    @Schema(description = "봉사 모집글 내용", example = "서울 청계천 주변 환경 미화 봉사 모집합니다. <br>")
-    @NotBlank(message = "모집글 내용은 필수 값입니다.")
-    String content,
-    @Schema(description = "예상 모집 인원", example = "4")
-    @NotNull(message = "예상 모집 인원은 필수 값입니다.")
-    Integer recruitmentCount,
-    @Schema(description = "봉사 시작 일시", example = "2024-11-20T10:00:00")
-    @NotNull(message = "봉사 시작 일시는 필수 값입니다.")
-    LocalDateTime volunteerStartDateTime,
-    @Schema(description = "봉사 종료 일시", example = "2024-11-20T12:00:00")
-    @NotNull(message = "봉사 종료 일시는 필수 값입니다.")
-    LocalDateTime volunteerEndDateTime,
-    @Schema(description = "봉사 활동 유형", example = "ENVIRONMENTAL_PROTECTION")
-    @NotNull(message = "봉사 활동 유형은 필수 값입니다.")
-    VolunteerCategory volunteerCategory,
-    @Schema(description = "봉사 시간 인정 여부", example = "true")
-    @NotNull(message = "시간 인정 여부는 필수 값입니다.")
-    Boolean admitted
+        @Schema(description = "봉사 모집글 제목", example = "서울 청계천 환경 미화 봉사 모집")
+        @NotBlank(message = "모집글 제목은 필수 값입니다.")
+        String title,
+        @Schema(description = "봉사 모집글 내용", example = "서울 청계천 주변 환경 미화 봉사 모집합니다. <br>")
+        @NotBlank(message = "모집글 내용은 필수 값입니다.")
+        String content,
+        @Schema(description = "예상 모집 인원", example = "4")
+        @NotNull(message = "예상 모집 인원은 필수 값입니다.")
+        Integer recruitmentCount,
+        @Schema(description = "봉사 시작 일시", example = "2024-11-20T10:00:00", type = "string")
+        @NotNull(message = "봉사 시작 일시는 필수 값입니다.")
+        LocalDateTime volunteerStartDateTime,
+        @Schema(description = "봉사 종료 일시", example = "2024-11-20T12:00:00", type = "string")
+        @NotNull(message = "봉사 종료 일시는 필수 값입니다.")
+        LocalDateTime volunteerEndDateTime,
+        @Schema(description = "봉사 활동 유형", example = "ENVIRONMENTAL_PROTECTION")
+        @NotNull(message = "봉사 활동 유형은 필수 값입니다.")
+        VolunteerCategory volunteerCategory,
+        @Schema(description = "봉사 시간 인정 여부", example = "true")
+        @NotNull(message = "시간 인정 여부는 필수 값입니다.")
+        Boolean admitted
 ) {
 
 }
