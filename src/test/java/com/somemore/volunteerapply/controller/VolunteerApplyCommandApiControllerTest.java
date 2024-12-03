d0511e9dabc9c6e8eddc3e61c2dd80042f9c85fb
leebs0521 <leebs0521@gmail.com>
2024-12-03 15:39:43 +0900

    test(volunteer-apply): 봉사 지원 철회 기능 API 테스트

--- src/test/java/com/somemore/volunteerapply/controller/VolunteerApplyCommandApiControllerTest.java
+++ src/test/java/com/somemore/volunteerapply/controller/VolunteerApplyCommandApiControllerTest.java
@@ -2,7 +2,9 @@ package com.somemore.volunteerapply.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,6 +14,7 @@ import com.somemore.ControllerTestSupport;
 import com.somemore.WithMockCustomUser;
 import com.somemore.volunteerapply.dto.VolunteerApplyCreateRequestDto;
 import com.somemore.volunteerapply.usecase.ApplyVolunteerApplyUseCase;
+import com.somemore.volunteerapply.usecase.WithdrawVolunteerApplyUseCase;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,6 +32,9 @@ class VolunteerApplyCommandApiControllerTest extends ControllerTestSupport {
 
     @MockBean
     private ApplyVolunteerApplyUseCase applyVolunteerApplyUseCase;
+
+    @MockBean
+    private WithdrawVolunteerApplyUseCase withdrawVolunteerApplyUseCase;
 
     @Test
     @DisplayName("봉사 활동 지원 성공 테스트")
@@ -56,5 +62,25 @@ class VolunteerApplyCommandApiControllerTest extends ControllerTestSupport {
                 .andExpect(jsonPath("$.code").value(201))
                 .andExpect(jsonPath("$.data").value(applyId))
                 .andExpect(jsonPath("$.message").value("봉사 활동 지원 성공"));
+    }
+
+    @Test
+    @DisplayName("봉사 활동 철회 성공 테스트")
+    @WithMockCustomUser
+    void withdraw () throws Exception {
+        // given
+        Long id = 1L;
+
+        willDoNothing().given(withdrawVolunteerApplyUseCase)
+                .withdraw(any(), any(UUID.class));
+
+        // when
+        mockMvc.perform(delete("/api/volunteer-apply/{id}", id)
+                        .header("Authorization", "Bearer access-token"))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.data").value(""))
+                .andExpect(jsonPath("$.message").value("봉사 활동 지원 철회 성공"));
     }
 }
