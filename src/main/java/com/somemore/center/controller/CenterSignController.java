62560c3598ead4292dfb74327a07d57bf713ad7c
leebs0521 <leebs0521@gmail.com>
2024-12-03 09:29:11 +0900

    feat(center-login): 기관 로그인 스웨거 명세 추가

--- src/main/java/com/somemore/center/controller/CenterSignController.java
+++ src/main/java/com/somemore/center/controller/CenterSignController.java
@@ -1,24 +1,36 @@
 package com.somemore.center.controller;
 
 import com.somemore.auth.signout.usecase.SignOutUseCase;
+import com.somemore.center.dto.request.CenterSignRequestDto;
 import com.somemore.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/center")
-@Tag(name = "center Sign API", description = "기관 로그아웃")
+@Tag(name = "center Sign API", description = "기관 로그인, 로그아웃")
 public class CenterSignController {
 
     private final SignOutUseCase signOutUseCase;
+
+    @PostMapping("/sign-in")
+    public ApiResponse<String> signIn(
+            @RequestBody CenterSignRequestDto requestDto
+    ) {
+
+        return ApiResponse.ok("로그인되었습니다.");
+    }
 
     @PostMapping("/sign-out")
     public ApiResponse<String> signOut(
