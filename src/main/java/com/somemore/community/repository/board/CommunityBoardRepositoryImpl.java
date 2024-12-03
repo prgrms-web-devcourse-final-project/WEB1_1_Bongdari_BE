64fcb66ea6df3a25846d3b55692103abdc574481
ayoung-dev <dkdudab@hanmail.net>
2024-12-03 11:15:20 +0900

    chore(community): 불필요한 import 제거

--- src/main/java/com/somemore/community/repository/board/CommunityBoardRepositoryImpl.java
+++ src/main/java/com/somemore/community/repository/board/CommunityBoardRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.somemore.community.repository.board;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.somemore.community.domain.CommunityBoard;
