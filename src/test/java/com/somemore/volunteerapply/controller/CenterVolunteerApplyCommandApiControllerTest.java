490fc7b469145a0d7250e021a2a9768d8de83ace
leebs0521 <leebs0521@gmail.com>
2024-12-03 21:01:44 +0900

    test(volunteer-apply): 봉사 활동 지원 거절 API 테스트

--- src/test/java/com/somemore/volunteerapply/controller/CenterVolunteerApplyCommandApiControllerTest.java
+++ src/test/java/com/somemore/volunteerapply/controller/CenterVolunteerApplyCommandApiControllerTest.java
@@ -1,16 +1,15 @@
 package com.somemore.volunteerapply.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.somemore.ControllerTestSupport;
 import com.somemore.WithMockCustomUser;
 import com.somemore.volunteerapply.usecase.ApproveVolunteerApplyUseCase;
+import com.somemore.volunteerapply.usecase.RejectVolunteerApplyUseCase;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,11 +22,11 @@ class CenterVolunteerApplyCommandApiControllerTest extends ControllerTestSupport
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @MockBean
     private ApproveVolunteerApplyUseCase approveVolunteerApplyUseCase;
+
+    @MockBean
+    private RejectVolunteerApplyUseCase rejectVolunteerApplyUseCase;
 
     @Test
     @DisplayName("봉사 활동 지원 승인 성공 테스트")
@@ -46,5 +45,24 @@ class CenterVolunteerApplyCommandApiControllerTest extends ControllerTestSupport
                 .andExpect(jsonPath("$.code").value(200))
                 .andExpect(jsonPath("$.data").value(""))
                 .andExpect(jsonPath("$.message").value("봉사 활동 지원 승인 성공"));
+    }
+
+    @Test
+    @DisplayName("봉사 활동 지원 거절 성공 테스트")
+    @WithMockCustomUser(role = "CENTER")
+    void reject() throws Exception {
+        // given
+        Long id = 1L;
+
+        willDoNothing().given(rejectVolunteerApplyUseCase)
+                .reject(any(), any(UUID.class));
+        // when
+        mockMvc.perform(patch("/api/volunteer-apply/{id}/reject", id)
+                        .header("Authorization", "Bearer access-token"))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.data").value(""))
+                .andExpect(jsonPath("$.message").value("봉사 활동 지원 거절 성공"));
     }
 }
