6ae9e3c0883bf28258231430fcf1f3e7beb0e8c1
leebs0521 <leebs0521@gmail.com>
2024-12-04 12:21:14 +0900

    test(volunteer-apply): 봉사 지원 단건 조회 API 테스트

--- src/test/java/com/somemore/volunteerapply/controller/VolunteerApplyQueryApiControllerTest.java
+++ src/test/java/com/somemore/volunteerapply/controller/VolunteerApplyQueryApiControllerTest.java
@@ -1,5 +1,6 @@
 package com.somemore.volunteerapply.controller;
 
+import static com.somemore.volunteerapply.domain.ApplyStatus.WAITING;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -7,8 +8,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.somemore.ControllerTestSupport;
+import com.somemore.volunteerapply.dto.response.VolunteerApplyResponseDto;
 import com.somemore.volunteerapply.dto.response.VolunteerApplySummaryResponseDto;
 import com.somemore.volunteerapply.usecase.VolunteerApplyQueryUseCase;
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +26,40 @@ class VolunteerApplyQueryApiControllerTest extends ControllerTestSupport {
     @MockBean
     private VolunteerApplyQueryUseCase volunteerApplyQueryUseCase;
 
+    @DisplayName("특정 모집글 봉사자 지원 단건 조회 성공 테스트")
+    @Test
+    void getVolunteerApplyByRecruitIdAndVolunteerId() throws Exception {
+        // given
+        Long recruitBoardId = 1L;
+        UUID volunteerId = UUID.randomUUID();
+
+        VolunteerApplyResponseDto response = VolunteerApplyResponseDto.builder()
+                .id(1L)
+                .volunteerId(volunteerId)
+                .recruitBoardId(recruitBoardId)
+                .status(WAITING)
+                .attended(false)
+                .build();
+
+        given(volunteerApplyQueryUseCase.getVolunteerApplyByRecruitIdAndVolunteerId(recruitBoardId,
+                volunteerId))
+                .willReturn(response);
+
+        // when & then
+        mockMvc.perform(
+                        get("/api/volunteer-apply/recruit-board/{recruitBoardId}/volunteer/{volunteerId}",
+                                recruitBoardId, volunteerId)
+                                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.message").value("특정 모집글에 대한 봉사자 지원 단건 조회 성공"))
+                .andExpect(jsonPath("$.data.id").value(1))
+                .andExpect(jsonPath("$.data.volunteer_id").value(volunteerId.toString()))
+                .andExpect(jsonPath("$.data.recruit_board_id").value(recruitBoardId))
+                .andExpect(jsonPath("$.data.status").value("WAITING"))
+                .andExpect(jsonPath("$.data.attended").value(false));
+    }
+
     @DisplayName("모집글 지원자 통계 조회 성공 테스트")
     @Test
     void getSummaryByRecruitBoardId() throws Exception {
@@ -35,7 +72,7 @@ class VolunteerApplyQueryApiControllerTest extends ControllerTestSupport {
                 .reject(2L)
                 .build();
 
-        given(volunteerApplyQueryUseCase.getSummaryByRecruitBoardId(recruitBoardId))
+        given(volunteerApplyQueryUseCase.getSummaryByRecruitId(recruitBoardId))
                 .willReturn(response);
 
         // when & then
