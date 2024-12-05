195b929f1d0b934c6d5e4b070dd8b9e362e8233a
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 15:32:13 +0900

    feat(NotificationRepository): 알림 조회 추가
    - 읽음, 읽지 않음 조건에 따라서 알림 조회 추가.

--- src/main/java/com/somemore/notification/repository/NotificationRepositoryImpl.java
+++ src/main/java/com/somemore/notification/repository/NotificationRepositoryImpl.java
@@ -1,17 +1,54 @@
 package com.somemore.notification.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.somemore.notification.domain.Notification;
+import com.somemore.notification.domain.QNotification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 @RequiredArgsConstructor
-public class NotificationRepositoryImpl implements NotificationRepository{
+public class NotificationRepositoryImpl implements NotificationRepository {
 
     private final NotificationJpaRepository notificationJpaRepository;
+    private final JPAQueryFactory queryFactory;
+
+    private static final QNotification notification = QNotification.notification;
 
     @Override
     public Notification save(Notification notification) {
         return notificationJpaRepository.save(notification);
+    }
+
+    @Override
+    public List<Notification> findByReceiverIdAndUnread(UUID receiverId) {
+        return queryFactory.selectFrom(notification)
+                .where(eqReceiverId(receiverId),
+                        isUnread())
+                .fetch();
+    }
+
+    @Override
+    public List<Notification> findByReceiverIdAndRead(UUID receiverId) {
+        return queryFactory.selectFrom(notification)
+                .where(eqReceiverId(receiverId),
+                        isRead())
+                .fetch();
+    }
+
+    private static BooleanExpression eqReceiverId(UUID userId) {
+        return notification.receiverId.eq(userId);
+    }
+
+    private static BooleanExpression isUnread() {
+        return notification.read.eq(false);
+    }
+
+    private static BooleanExpression isRead() {
+        return notification.read.eq(true);
     }
 }
