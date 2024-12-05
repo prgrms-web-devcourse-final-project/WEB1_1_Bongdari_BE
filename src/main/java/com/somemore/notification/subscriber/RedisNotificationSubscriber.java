a46eba05828895c219583e6e95ff48e9c8dd05b9
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 22:21:35 +0900

    feat(RedisNotificationSubscriber): 메시지 컨버팅 책임 추가

--- src/main/java/com/somemore/notification/subscriber/RedisNotificationSubscriber.java
+++ src/main/java/com/somemore/notification/subscriber/RedisNotificationSubscriber.java
@@ -1,5 +1,7 @@
 package com.somemore.notification.subscriber;
 
+import com.somemore.notification.converter.MessageConverter;
+import com.somemore.notification.domain.Notification;
 import com.somemore.notification.handler.NotificationHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
@@ -11,11 +13,14 @@ import org.springframework.stereotype.Component;
 public class RedisNotificationSubscriber implements MessageListener {
 
     private final NotificationHandler notificationHandler;
+    private final MessageConverter messageConverter;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        String data = new String(message.getBody());
+        Notification notification = messageConverter.ToNotification(
+                new String(message.getBody())
+        );
 
-        notificationHandler.handle(data);
+        notificationHandler.handle(notification);
     }
 }
