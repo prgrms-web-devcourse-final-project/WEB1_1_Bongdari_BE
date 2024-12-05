f946460bbb0bbf4dfff870eca50e687f5db41f3f
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 09:59:32 +0900

    refactor(RedisConfig): 패키지 이동

--- src/main/java/com/somemore/global/redis/config/RedisConfig.java
+++ src/main/java/com/somemore/global/redis/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.somemore.global.configure;
+package com.somemore.global.redis.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
