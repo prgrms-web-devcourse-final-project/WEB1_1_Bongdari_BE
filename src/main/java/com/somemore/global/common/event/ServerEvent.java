1c20222cb1d89417bb6ca04cbf0a8002296635f8
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 22:13:23 +0900

    feat(ServerEvent): subType 추가, 직렬화를 위한 JsonProperty 추가

--- src/main/java/com/somemore/global/common/event/ServerEvent.java
+++ src/main/java/com/somemore/global/common/event/ServerEvent.java
@@ -1,6 +1,6 @@
 package com.somemore.global.common.event;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
@@ -8,10 +8,24 @@ import java.time.LocalDateTime;
 
 @Getter
 @SuperBuilder
-public abstract class ServerEvent {
+public abstract class ServerEvent<T extends Enum<T>> {
 
+    @JsonProperty("type")
     private final ServerEventType type;
 
-    @Builder.Default
-    private final LocalDateTime createdAt = LocalDateTime.now();
+    @JsonProperty("subType")
+    private final T subType;
+
+    @JsonProperty("createdAt")
+    private final LocalDateTime createdAt;
+
+    protected ServerEvent(
+            @JsonProperty("type") ServerEventType type,
+            @JsonProperty("subType") T subType,
+            @JsonProperty("createdAt") LocalDateTime createdAt
+    ) {
+        this.type = type;
+        this.subType = subType;
+        this.createdAt = (createdAt == null) ? LocalDateTime.now() : createdAt;
+    }
 }
