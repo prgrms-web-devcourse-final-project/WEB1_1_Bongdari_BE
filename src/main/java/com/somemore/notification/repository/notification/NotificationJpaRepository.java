a2b59eae9d179fcd56999c92021212bf7fad25e6
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 09:55:52 +0900

    refactor(NotificationRepository): 패키지 이동

--- src/main/java/com/somemore/notification/repository/notification/NotificationJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.somemore.notification.repository.notification;
-
-import com.somemore.notification.domain.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationJpaRepository extends JpaRepository<Notification, Long> {
-}
