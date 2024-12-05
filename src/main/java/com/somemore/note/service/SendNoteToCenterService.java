7d5e6f992b388fe456b57c73d59373976744a137
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-12-05 17:02:16 +0900

    feat: 봉사자 to 기관 쪽지 송신 기능 서비스레이어 구현
    - 유스케이스 생성
    - 송신 클래스및 메서드 생성
    - 요청 Dto 구현및 toEntity 변환 메서드 구

---
+++ src/main/java/com/somemore/note/service/SendNoteToCenterService.java
@@ -0,0 +1,32 @@
+package com.somemore.note.service;
+
+import com.somemore.center.usecase.query.CenterQueryUseCase;
+import com.somemore.note.domain.Note;
+import com.somemore.note.dto.SendNoteToCenterRequestDto;
+import com.somemore.note.repository.NoteRepository;
+import com.somemore.note.usecase.SendNoteToCenterUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class SendNoteToCenterService implements SendNoteToCenterUseCase {
+
+    private final NoteRepository noteRepository;
+    private final CenterQueryUseCase centerQueryUseCase;
+
+    @Override
+    public Long sendNoteToCenter(UUID senderId, SendNoteToCenterRequestDto requestDto) {
+
+        centerQueryUseCase.validateCenterExists(requestDto.receiverId());
+
+        Note note = requestDto.toEntity(senderId);
+        noteRepository.save(note);
+        return note.getId();
+    }
+
+}
