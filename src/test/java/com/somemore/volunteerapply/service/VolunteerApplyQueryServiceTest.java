cf8e1c35c06aec0bf6cc2ec51b1b96c73e85c4ad
leebs0521 <leebs0521@gmail.com>
2024-12-05 09:34:11 +0900

    test(volunteer-apply): facade 에서 레포지토리가 아닌 UseCase를 의존하도록 변경에 따른 테스트

--- src/test/java/com/somemore/volunteerapply/service/VolunteerApplyQueryServiceTest.java
+++ src/test/java/com/somemore/volunteerapply/service/VolunteerApplyQueryServiceTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.somemore.IntegrationTestSupport;
 import com.somemore.volunteerapply.domain.ApplyStatus;
 import com.somemore.volunteerapply.domain.VolunteerApply;
+import com.somemore.volunteerapply.dto.condition.VolunteerApplySearchCondition;
 import com.somemore.volunteerapply.dto.response.VolunteerApplyResponseDto;
 import com.somemore.volunteerapply.dto.response.VolunteerApplySummaryResponseDto;
 import com.somemore.volunteerapply.repository.VolunteerApplyRepository;
@@ -16,6 +17,9 @@ import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -118,6 +122,51 @@ class VolunteerApplyQueryServiceTest extends IntegrationTestSupport {
         assertThat(dto.volunteerId()).isEqualTo(volunteerId);
     }
 
+    @DisplayName("모집글 아이디로 지원 리스트를 페이징 조회할 수 있다.")
+    @Test
+    void getAllByRecruitId() {
+        // given
+        Long boardId = 1L;
+        VolunteerApply apply1 = createApply(UUID.randomUUID(), boardId);
+        VolunteerApply apply2 = createApply(UUID.randomUUID(), boardId);
+        volunteerApplyRepository.saveAll(List.of(apply1, apply2));
+
+        VolunteerApplySearchCondition condition = VolunteerApplySearchCondition.builder()
+                .pageable(getPageable())
+                .build();
+
+        // when
+        Page<VolunteerApply> applies = volunteerApplyQueryService.getAllByRecruitId(boardId,
+                condition);
+
+        // then
+        assertThat(applies).hasSize(2);
+    }
+
+    @DisplayName("봉사자 아이디로 봉사 지원 리스트를 페이징 조회 할 수있다.")
+    @Test
+    void getVolunteerAppliesByVolunteerId() {
+        // given
+        UUID volunteerId = UUID.randomUUID();
+
+        VolunteerApply apply1 = createApply(volunteerId, 200L);
+        VolunteerApply apply2 = createApply(volunteerId, 201L);
+        VolunteerApply apply3 = createApply(volunteerId, 202L);
+        VolunteerApply apply4 = createApply(UUID.randomUUID(), 203L);
+        volunteerApplyRepository.saveAll(List.of(apply1, apply2, apply3, apply4));
+
+        VolunteerApplySearchCondition condition = VolunteerApplySearchCondition.builder()
+                .pageable(getPageable())
+                .build();
+
+        // when
+        Page<VolunteerApply> applies = volunteerApplyQueryService.getAllByVolunteerId(
+                volunteerId, condition);
+
+        // then
+        assertThat(applies).hasSize(3);
+    }
+
     private VolunteerApply createApply(UUID volunteerId, Long recruitId) {
         return createApply(volunteerId, recruitId, WAITING);
     }
@@ -129,5 +178,9 @@ class VolunteerApplyQueryServiceTest extends IntegrationTestSupport {
                 .status(status)
                 .attended(false)
                 .build();
+    }
+
+    private Pageable getPageable() {
+        return PageRequest.of(0, 4);
     }
 }
