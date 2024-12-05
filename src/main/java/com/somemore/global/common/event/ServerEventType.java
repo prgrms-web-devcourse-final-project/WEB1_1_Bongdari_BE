66fa6bb11f4f2b7eeeb1ea98ec95b606c20266b8
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 22:14:32 +0900

    feat(ServerEventType): ServerEventType의 하위 타입(subType) 추가, 직렬화를 위한 JsonProperty 추가, subType Getter 추가

--- src/main/java/com/somemore/global/common/event/ServerEventType.java
+++ src/main/java/com/somemore/global/common/event/ServerEventType.java
@@ -1,5 +1,28 @@
 package com.somemore.global.common.event;
 
+import com.somemore.notification.domain.NotificationSubType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Getter
 public enum ServerEventType {
-    NOTIFICATION
+    NOTIFICATION(NotificationSubType.class)
+    ;
+
+    private final Class<? extends Enum<?>> subtype;
+
+    public static ServerEventType from(String value) {
+        return Arrays.stream(ServerEventType.values())
+                .filter(type -> type.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 이벤트 타입입니다: " + value));
+    }
+
+    public Set<? extends Enum<?>> getSubtypes() {
+        return Set.of(subtype.getEnumConstants());
+    }
 }
