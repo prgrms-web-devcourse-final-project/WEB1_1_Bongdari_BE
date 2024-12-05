799139fef46daf8de37cce555f011ba9a9f60df8
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 10:52:58 +0900

    feat(NotificationType): 알림 타입 추가
    - 설명 필드 추가.
    - 정적 팩토리 메서드 from 추가.

--- src/main/java/com/somemore/notification/domain/NotificationType.java
+++ src/main/java/com/somemore/notification/domain/NotificationType.java
@@ -1,7 +1,22 @@
 package com.somemore.notification.domain;
 
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@RequiredArgsConstructor
 public enum NotificationType {
-    NOTE,
-    REVIEW,
-    RECRUIT
+    NOTE_BLAH_BLAH("쪽지"),
+    REVIEW_BLAH_BLAH("후기 요청"),
+    VOLUNTEER_APPLY_STATUS_CHANGE("신청 상태 변경")
+    ;
+
+    private final String description;
+
+    public static NotificationType from(String value) {
+        return Arrays.stream(NotificationType.values())
+                .filter(type -> type.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 타입입니다: " + value));
+    }
 }
