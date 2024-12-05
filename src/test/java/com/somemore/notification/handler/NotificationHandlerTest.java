c8db817a987f2c552c3e1fdb0ed32c83712bd63a
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-05 22:27:06 +0900

    test(NotificationHandler): 알림 핸들링 테스트 추가
    - 저장 위주

---
+++ src/test/java/com/somemore/notification/handler/NotificationHandlerTest.java
@@ -0,0 +1,65 @@
+package com.somemore.notification.handler;
+
+import com.somemore.IntegrationTestSupport;
+import com.somemore.notification.converter.MessageConverter;
+import com.somemore.notification.domain.Notification;
+import com.somemore.notification.domain.NotificationSubType;
+import com.somemore.notification.repository.NotificationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+class NotificationHandlerTest extends IntegrationTestSupport {
+
+    @Autowired
+    private NotificationHandlerImpl notificationHandler;
+
+    @Autowired
+    private MessageConverter messageConverter;
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Test
+    @DisplayName("handle 호출 시 Notification이 저장된다")
+    void handle() {
+        // given
+        String message = """
+                {
+                    "type": "NOTIFICATION",
+                    "subType": "VOLUNTEER_APPLY_STATUS_CHANGE",
+                    "receiverId": "123e4567-e89b-12d3-a456-426614174000",
+                    "volunteerApplyId": 123,
+                    "centerId": "123e4567-e89b-12d3-a456-426614174001",
+                    "recruitBoardId": 456,
+                    "newStatus": "APPROVED",
+                    "createdAt": "2024-12-05T10:00:00"
+                }
+                """;
+
+        UUID receiverId = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+
+        Notification notification = messageConverter.ToNotification(message);
+        // when
+        notificationHandler.handle(notification);
+
+        // then
+        List<Notification> notifications = notificationRepository.findByReceiverIdAndUnread(receiverId);
+        assertThat(notifications).hasSize(1);
+
+        Notification savedNotification = notifications.getFirst();
+        assertThat(savedNotification.getReceiverId()).isEqualTo(receiverId);
+        assertThat(savedNotification.getTitle()).isEqualTo("봉사 활동 신청이 승인되었습니다.");
+        assertThat(savedNotification.getType()).isEqualTo(NotificationSubType.VOLUNTEER_APPLY_STATUS_CHANGE);
+        assertThat(savedNotification.getRelatedId()).isEqualTo(456L); // 프론트 요구사항: 123L(봉사신청아이디), 456L(모집글아이디)
+        assertThat(savedNotification.isRead()).isFalse();
+        assertThat(savedNotification.getCreatedAt()).isEqualTo(notification.getCreatedAt());
+    }
+}
