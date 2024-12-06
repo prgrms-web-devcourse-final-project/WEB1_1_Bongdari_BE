0a3d3bf471096eab81c33d4d6bdb574753ecbe0e
Beom-Su Lee <leebs0521@gmail.com>
2024-12-06 15:24:57 +0900

    refactor: 지원 내역이 없는 경우 210을 반환하도록 변경 (#169)

--- src/main/java/com/somemore/global/common/response/ApiResponse.java
+++ src/main/java/com/somemore/global/common/response/ApiResponse.java
@@ -19,6 +19,10 @@ public class ApiResponse<T> {
         return new ApiResponse<>(status, message, data);
     }
 
+    public static ApiResponse<String> ok(int status, String message) {
+        return new ApiResponse<>(status, message, "");
+    }
+
     public static ApiResponse<String> ok(String message) {
         return new ApiResponse<>(200, message, "");
     }
