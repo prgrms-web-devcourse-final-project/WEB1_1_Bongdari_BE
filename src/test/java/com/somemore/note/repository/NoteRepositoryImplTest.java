bde00e4d7de0f3e1f78ad7fc2f8562850805df93
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-12-08 01:18:07 +0900

    feat: 기관의 자신에게 온 쪽지 상세 내용 확인 기능 쿼리 메서드 구현
    - 쿼리 메서드 생성
    - mapper record 클래 생성
    - 테스트 코드 작성및 검증 완료

--- src/test/java/com/somemore/note/repository/NoteRepositoryImplTest.java
+++ src/test/java/com/somemore/note/repository/NoteRepositoryImplTest.java
@@ -4,6 +4,7 @@ import com.somemore.IntegrationTestSupport;
 import com.somemore.center.domain.Center;
 import com.somemore.center.repository.CenterJpaRepository;
 import com.somemore.note.domain.Note;
+import com.somemore.note.repository.mapper.NoteDetailViewForCenter;
 import com.somemore.note.repository.mapper.NoteReceiverViewForCenter;
 import com.somemore.note.repository.mapper.NoteReceiverViewForVolunteer;
 import com.somemore.volunteer.domain.Volunteer;
@@ -16,6 +17,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static com.somemore.auth.oauth.OAuthProvider.NAVER;
@@ -69,26 +71,44 @@ class NoteRepositoryImplTest extends IntegrationTestSupport {
         assertThat(result.getContent().getFirst().title()).isEqualTo("Note 9");
     }
 
+    @DisplayName("기관은 자신에게 온 쪽지의 상세 내용을 확인할 수 있다 (Repository)")
+    @Test
+    void findNoteDetailViewReceiverIsCenter() {
+        // given
+        Center center = createCenter();
+        Volunteer volunteer = createVolunteer();
+        Note note = createNote(center.getId(), volunteer.getId());
+
+        // when
+        Optional<NoteDetailViewForCenter> result = noteRepository.findNoteDetailViewReceiverIsCenter(note.getId());
+
+        // then
+        assertThat(result).isPresent();
+
+        NoteDetailViewForCenter detailView = result.get();
+
+        assertThat(detailView.noteId()).isEqualTo(note.getId());
+        assertThat(detailView.title()).isEqualTo(note.getTitle());
+        assertThat(detailView.content()).isEqualTo(note.getContent());
+        assertThat(detailView.senderId()).isEqualTo(volunteer.getId());
+        assertThat(detailView.senderName()).isEqualTo(volunteer.getNickname());
+        assertThat(detailView.senderProfileImgLink()).isEqualTo(volunteer.getImgUrl());
+        assertThat(detailView.createdAt()).isEqualTo(note.getCreatedAt());
+    }
+
+    private Note createNote(UUID receiverId, UUID senderId){
+        Note note = Note.create(senderId, receiverId, "제목", "내용");
+        noteRepository.save(note);
+
+        return note;
+    }
+
     private void createTestNotesForCenter(UUID centerId) {
         for (int i = 1; i <= 15; i++) {
             Volunteer volunteer = createVolunteer();
             volunteerJpaRepository.save(volunteer);
 
             Note note = Note.create(volunteer.getId(), centerId, "Note " + i, "내용");
-            noteRepository.save(note);
-        }
-    }
-
-    private Volunteer createVolunteer() {
-        Volunteer volunteer = Volunteer.createDefault(NAVER, UUID.randomUUID().toString());
-        return volunteerJpaRepository.save(volunteer);
-    }
-
-    private void createTestNotesForVolunteer(UUID volunteerId) {
-        for (int i = 1; i <= 15; i++) {
-            Center center = createCenter();
-
-            Note note = Note.create(center.getId(), volunteerId, "Note " + i, "내용");
             noteRepository.save(note);
         }
     }
@@ -107,6 +127,20 @@ class NoteRepositoryImplTest extends IntegrationTestSupport {
         centerJpaRepository.save(center);
 
         return center;
+    }
+
+    private Volunteer createVolunteer() {
+        Volunteer volunteer = Volunteer.createDefault(NAVER, UUID.randomUUID().toString());
+        return volunteerJpaRepository.save(volunteer);
+    }
+
+    private void createTestNotesForVolunteer(UUID volunteerId) {
+        for (int i = 1; i <= 15; i++) {
+            Center center = createCenter();
+
+            Note note = Note.create(center.getId(), volunteerId, "Note " + i, "내용");
+            noteRepository.save(note);
+        }
     }
 
 }
