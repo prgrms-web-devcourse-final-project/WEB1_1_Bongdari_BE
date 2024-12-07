d9661c5b29cd06851c3f550f7f39734f93cd44af
조재중 <9gle3492@gmail.com>
2024-12-07 21:07:45 +0900

    feat: cookie의 accessToken 추출 (#185)
    * feat(accessToken): cookie의 accessToken 추출 가능
    
    - 쿠키와 헤더 모두에서 토큰을 추출할 수 있음.
      - 쿠키에서 토큰을 초기화하지 못하면 헤더에서 토큰을 찾음.
      - 헤더에서 토큰을 찾는 이유는 스웨거, 디버깅.
    
    - accessToken 리팩토링
    
    - 쿠키 NPE 예방.
    
    * refactor(accessToken): 코드 추상화 수준 맞추기
    
    - NPE 처리 추가.

--- src/main/java/com/somemore/auth/jwt/domain/EncodedToken.java
+++ src/main/java/com/somemore/auth/jwt/domain/EncodedToken.java
@@ -1,4 +1,17 @@
 package com.somemore.auth.jwt.domain;
 
 public record EncodedToken(String value) {
+
+    public boolean isUninitialized() {
+        return value == null
+                || value.isEmpty()
+                || value.equals("UNINITIALIZED");
+    }
+
+    public EncodedToken removePrefix(String prefix) {
+        if (this.value.startsWith(prefix)) {
+            return new EncodedToken(this.value.substring(prefix.length()));
+        }
+        return this;
+    }
 }
