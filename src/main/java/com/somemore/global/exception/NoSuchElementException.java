5046781eb31d4eb150036fa849f165d27afcfd42
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-12-08 01:41:25 +0900

    feat: NoSuchElement 예외 추가
    - NoSuchElementException 클래스 추가
    - 예외메세지 추가
    - 전역 예외처리 핸들러에 NoSuchElementException 추가

---
+++ src/main/java/com/somemore/global/exception/NoSuchElementException.java
@@ -0,0 +1,7 @@
+package com.somemore.global.exception;
+
+public class NoSuchElementException extends RuntimeException{
+    public NoSuchElementException(final String message) {
+        super(message);
+    }
+}
