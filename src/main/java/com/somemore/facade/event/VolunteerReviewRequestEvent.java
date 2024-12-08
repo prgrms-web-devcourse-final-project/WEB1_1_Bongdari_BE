199166487f61fc3770cbaf246413edb6aa95de0e
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-08 19:09:43 +0900

    feat(VolunteerReviewRequestEvent): 봉사 후기 요청 이벤트 추가

---
+++ src/main/java/com/somemore/facade/event/VolunteerReviewRequestEvent.java
@@ -0,0 +1,35 @@
+package com.somemore.facade.event;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.somemore.global.common.event.ServerEvent;
+import com.somemore.global.common.event.ServerEventType;
+import com.somemore.notification.domain.NotificationSubType;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@SuperBuilder
+public class VolunteerReviewRequestEvent extends ServerEvent<NotificationSubType> {
+    private final UUID volunteerId;
+    private final Long volunteerApplyId;
+    private final UUID centerId;
+    private final Long recruitBoardId;
+
+    @JsonCreator
+    public VolunteerReviewRequestEvent(
+            @JsonProperty("receiverId") UUID volunteerId,
+            @JsonProperty("volunteerApplyId") Long volunteerApplyId,
+            @JsonProperty("centerId") UUID centerId,
+            @JsonProperty("recruitBoardId") Long recruitBoardId
+    ) {
+        super(ServerEventType.NOTIFICATION, NotificationSubType.VOLUNTEER_APPLY_STATUS_CHANGE, LocalDateTime.now());
+        this.volunteerId = volunteerId;
+        this.volunteerApplyId = volunteerApplyId;
+        this.centerId = centerId;
+        this.recruitBoardId = recruitBoardId;
+    }
+}
