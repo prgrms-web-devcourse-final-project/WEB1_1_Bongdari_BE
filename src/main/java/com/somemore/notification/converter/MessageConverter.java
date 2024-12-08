93689928462c844a7f190f78d45104efdf45c8c7
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-08 20:59:14 +0900

    refactor: 예외 명시

--- src/main/java/com/somemore/notification/converter/MessageConverter.java
+++ src/main/java/com/somemore/notification/converter/MessageConverter.java
@@ -1,6 +1,7 @@
 package com.somemore.notification.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.somemore.facade.event.VolunteerReviewRequestEvent;
@@ -46,7 +47,7 @@ public class MessageConverter {
                 .build();
     }
 
-    private Notification buildVolunteerApplyStatusChangeNotification(String message) throws Exception {
+    private Notification buildVolunteerApplyStatusChangeNotification(String message) throws JsonProcessingException {
         VolunteerApplyStatusChangeEvent event = objectMapper.readValue(message, VolunteerApplyStatusChangeEvent.class);
 
         return Notification.builder()
