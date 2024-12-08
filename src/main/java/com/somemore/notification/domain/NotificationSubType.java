b6d1fa0d87956a0f2d4259096888ec674490572c
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-08 19:10:50 +0900

    feat(VolunteerReviewRequestEvent): 봉사 후기 요청 이벤트 발행 추가
    - SettleVolunteerApplyFacadeService 에서 이벤트 발생.
    - subType 명명 수정.

--- src/main/java/com/somemore/notification/domain/NotificationSubType.java
+++ src/main/java/com/somemore/notification/domain/NotificationSubType.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public enum NotificationSubType {
     NOTE_BLAH_BLAH("쪽지"),
-    REVIEW_BLAH_BLAH("후기 요청"),
+    VOLUNTEER_REVIEW_REQUEST("봉사 후기 요청"),
     VOLUNTEER_APPLY_STATUS_CHANGE("신청 상태 변경")
     ;
 
