3f0788a5940cdcdac11c6013b166cde3e738f374
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-09 17:08:27 +0900

    feat(CreateRecruitBoardService): publishCreateRecruitBoardEvent 추가
    - 기관이 봉사 모집을 등록하면 이벤트 발생.
      - 이는 봉사자의 관심 기관 봉사 모집 등록 알림을 위한 첫 단계.

--- src/main/java/com/somemore/recruitboard/service/command/CreateRecruitBoardService.java
+++ src/main/java/com/somemore/recruitboard/service/command/CreateRecruitBoardService.java
@@ -1,14 +1,19 @@
 package com.somemore.recruitboard.service.command;
 
+import com.somemore.global.common.event.DomainEventSubType;
+import com.somemore.global.common.event.ServerEventPublisher;
+import com.somemore.global.common.event.ServerEventType;
 import com.somemore.location.usecase.command.CreateLocationUseCase;
 import com.somemore.recruitboard.domain.RecruitBoard;
 import com.somemore.recruitboard.dto.request.RecruitBoardCreateRequestDto;
+import com.somemore.recruitboard.event.CreateRecruitBoardEvent;
 import com.somemore.recruitboard.repository.RecruitBoardRepository;
 import com.somemore.recruitboard.usecase.command.CreateRecruitBoardUseCase;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 
 @RequiredArgsConstructor
@@ -18,19 +23,31 @@ public class CreateRecruitBoardService implements CreateRecruitBoardUseCase {
 
     private final RecruitBoardRepository recruitBoardRepository;
     private final CreateLocationUseCase createLocationUseCase;
+    private final ServerEventPublisher serverEventPublisher;
 
     @Override
     public Long createRecruitBoard(
-        RecruitBoardCreateRequestDto requestDto,
-        UUID centerId,
-        String imgUrl
+            RecruitBoardCreateRequestDto requestDto,
+            UUID centerId,
+            String imgUrl
     ) {
         Long locationId = createLocationUseCase.createLocation(requestDto.location());
         RecruitBoard recruitBoard = requestDto.toEntity(centerId, locationId, imgUrl);
 
         recruitBoardRepository.save(recruitBoard);
 
+        publishCreateRecruitBoardEvent(centerId, recruitBoard);
         return recruitBoard.getId();
     }
 
+    private void publishCreateRecruitBoardEvent(UUID centerId, RecruitBoard recruitBoard) {
+        CreateRecruitBoardEvent event = CreateRecruitBoardEvent.builder()
+                .type(ServerEventType.DOMAIN_EVENT)
+                .subType(DomainEventSubType.CREATE_RECRUIT_BOARD)
+                .centerId(centerId)
+                .recruitBoardId(recruitBoard.getId())
+                .build();
+
+        serverEventPublisher.publish(event);
+    }
 }
