404ae84443a6e4524499b64b9aa35f2b801d1618
조재중 <9gle3492@gmail.com>
2024-12-09 12:16:51 +0900

    Feature/217 봉사 신청 알림 (#218)
    * feat(NotificationSubType): VOLUNTEER_APPLY 추가
    
    * feat(VolunteerApplyEvent): 봉사 신청 이벤트 추가
    
    * feat(ApplyVolunteerApplyService): 봉사 신청 이벤트 발행
    
    * feat(MessageConverter): 봉사 신청 이벤트 메시지 컨버팅 처리

--- src/main/java/com/somemore/volunteerapply/service/ApplyVolunteerApplyService.java
+++ src/main/java/com/somemore/volunteerapply/service/ApplyVolunteerApplyService.java
@@ -3,11 +3,15 @@ package com.somemore.volunteerapply.service;
 import static com.somemore.global.exception.ExceptionMessage.DUPLICATE_APPLICATION;
 import static com.somemore.global.exception.ExceptionMessage.RECRUITMENT_NOT_OPEN;
 
+import com.somemore.global.common.event.ServerEventPublisher;
+import com.somemore.global.common.event.ServerEventType;
 import com.somemore.global.exception.BadRequestException;
+import com.somemore.notification.domain.NotificationSubType;
 import com.somemore.recruitboard.domain.RecruitBoard;
 import com.somemore.recruitboard.usecase.query.RecruitBoardQueryUseCase;
 import com.somemore.volunteerapply.domain.VolunteerApply;
 import com.somemore.volunteerapply.dto.request.VolunteerApplyCreateRequestDto;
+import com.somemore.volunteerapply.event.VolunteerApplyEvent;
 import com.somemore.volunteerapply.repository.VolunteerApplyRepository;
 import com.somemore.volunteerapply.usecase.ApplyVolunteerApplyUseCase;
 import java.util.UUID;
@@ -22,6 +26,7 @@ public class ApplyVolunteerApplyService implements ApplyVolunteerApplyUseCase {
 
     private final VolunteerApplyRepository volunteerApplyRepository;
     private final RecruitBoardQueryUseCase recruitBoardQueryUseCase;
+    private final ServerEventPublisher serverEventPublisher;
 
     @Override
     public Long apply(VolunteerApplyCreateRequestDto requestDto, UUID volunteerId) {
@@ -32,6 +37,8 @@ public class ApplyVolunteerApplyService implements ApplyVolunteerApplyUseCase {
 
         VolunteerApply apply = requestDto.toEntity(volunteerId);
         volunteerApplyRepository.save(apply);
+
+        publishVolunteerApplyEvent(apply, board);
 
         return apply.getId();
     }
@@ -49,5 +56,18 @@ public class ApplyVolunteerApplyService implements ApplyVolunteerApplyUseCase {
         if (isDuplicate) {
             throw new BadRequestException(DUPLICATE_APPLICATION);
         }
+    }
+
+    private void publishVolunteerApplyEvent(VolunteerApply apply, RecruitBoard board) {
+        VolunteerApplyEvent event = VolunteerApplyEvent.builder()
+                .type(ServerEventType.NOTIFICATION)
+                .subType(NotificationSubType.VOLUNTEER_APPLY)
+                .volunteerId(apply.getVolunteerId())
+                .volunteerApplyId(apply.getId())
+                .centerId(board.getCenterId())
+                .recruitBoardId(board.getId())
+                .build();
+
+        serverEventPublisher.publish(event);
     }
 }
