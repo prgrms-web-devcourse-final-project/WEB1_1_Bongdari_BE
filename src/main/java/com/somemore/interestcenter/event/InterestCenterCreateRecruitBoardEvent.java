40731532f41d2c2075d072e955d97b83d9de1743
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-09 21:35:07 +0900

    feat(event): 역직렬화 지원

--- src/main/java/com/somemore/interestcenter/event/InterestCenterCreateRecruitBoardEvent.java
+++ src/main/java/com/somemore/interestcenter/event/InterestCenterCreateRecruitBoardEvent.java
@@ -1,10 +1,14 @@
 package com.somemore.interestcenter.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.somemore.global.common.event.ServerEvent;
+import com.somemore.global.common.event.ServerEventType;
 import com.somemore.notification.domain.NotificationSubType;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -13,4 +17,16 @@ public class InterestCenterCreateRecruitBoardEvent extends ServerEvent<Notificat
     private final UUID volunteerId;
     private final UUID centerId;
     private final Long recruitBoardId;
+
+    @JsonCreator
+    public InterestCenterCreateRecruitBoardEvent(
+            @JsonProperty(value = "volunteerId", required = true) UUID volunteerId,
+            @JsonProperty(value = "centerId", required = true) UUID centerId,
+            @JsonProperty(value = "recruitBoardId", required = true) Long recruitBoardId
+    ) {
+        super(ServerEventType.NOTIFICATION, NotificationSubType.INTEREST_CENTER_CREATE_RECRUIT_BOARD, LocalDateTime.now());
+        this.volunteerId = volunteerId;
+        this.centerId = centerId;
+        this.recruitBoardId = recruitBoardId;
+    }
 }
