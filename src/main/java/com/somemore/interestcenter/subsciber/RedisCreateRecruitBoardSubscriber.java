83b23f06374aabe9562bb0ca263e9e355bacea35
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-09 17:13:01 +0900

    feat(CreateRecruitBoard): CreateRecruitBoardEvent subscriber, converter 추가
    - 직렬화 된 이벤트 메시지를 이벤트로 컨버팅(역직렬화) 후 핸들러에게 전달

---
+++ src/main/java/com/somemore/interestcenter/subsciber/RedisCreateRecruitBoardSubscriber.java
@@ -0,0 +1,26 @@
+package com.somemore.interestcenter.subsciber;
+
+import com.somemore.interestcenter.converter.CreateRecruitBoardMessageConverter;
+import com.somemore.interestcenter.handler.CreateRecruitBoardHandler;
+import com.somemore.recruitboard.event.CreateRecruitBoardEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisCreateRecruitBoardSubscriber implements MessageListener {
+
+    private final CreateRecruitBoardHandler createRecruitBoardHandler;
+    private final CreateRecruitBoardMessageConverter messageConverter;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        CreateRecruitBoardEvent event = messageConverter.from(
+                new String(message.getBody())
+        );
+
+        createRecruitBoardHandler.handle(event);
+    }
+}
