c1bf380418c9a1fad0a34a08156486f386bccbb4
leebs0521 <leebs0521@gmail.com>
2024-12-22 16:40:17 +0900

    refactor(controller-test): ControllerTestSupport 리팩토링  - 컨트롤러 테스트시 print() 일괄 적용  - print()시 한글 깨짐 현상 해결

--- src/test/java/com/somemore/support/ControllerTestSupport.java
+++ src/test/java/com/somemore/support/ControllerTestSupport.java
@@ -1,12 +1,18 @@
 package com.somemore.support;
 
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -17,5 +23,16 @@ public abstract class ControllerTestSupport {
     protected MockMvc mockMvc;
 
     @Autowired
+    private WebApplicationContext ctx;
+
+    @Autowired
     protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
 }
