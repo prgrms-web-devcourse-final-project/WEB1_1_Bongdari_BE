4c38f74719527c28091de5bbf0ff5315614ba3cc
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-30 15:39:48 +0900

    feat(oauth): 인증 성공 핸들러 수정
    - 액세스 토큰 전달 방법 수정 (쿠키 -> 헤더).
    - 로직 개선

--- src/main/java/com/somemore/global/auth/oauth/handler/CustomOAuthSuccessHandler.java
+++ src/main/java/com/somemore/global/auth/oauth/handler/CustomOAuthSuccessHandler.java
@@ -1,12 +1,10 @@
 package com.somemore.global.auth.oauth.handler;
 
-import com.somemore.global.auth.cookie.CookieUseCase;
 import com.somemore.global.auth.jwt.domain.EncodedToken;
 import com.somemore.global.auth.jwt.domain.UserRole;
 import com.somemore.global.auth.jwt.usecase.GenerateTokensOnLoginUseCase;
 import com.somemore.global.auth.oauth.processor.OAuthUserProcessor;
 import com.somemore.global.auth.redirect.RedirectUseCase;
-import com.somemore.domains.volunteer.usecase.VolunteerQueryUseCase;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @Component
@@ -25,40 +23,43 @@ import java.util.UUID;
 @Slf4j
 public class CustomOAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final ProcessNaverOAuthUserService processNaverOAuthService;
-    private final VolunteerQueryUseCase volunteerQueryUseCase;
+    private final OAuthUserProcessor oauthUserProcessor;
     private final GenerateTokensOnLoginUseCase generateTokensOnLoginUseCase;
-    private final CookieUseCase cookieUseCase;
     private final RedirectUseCase redirectUseCase;
+
+    public static final String AUTHORIZATION = "Authorization";
 
     @Value("${app.front-url}")
     private String frontendRootUrl;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        String oAuthId;
-        switch (getOAuthProvider(authentication)) {
-            case NAVER -> oAuthId = processNaverOAuthService.processOAuthUser(authentication);
-            default -> {
-                log.error("지원하지 않는 OAuth 제공자입니다.");
-                throw new IllegalArgumentException();
-            }
-        }
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) {
+        OAuth2User oauthUser = extractOAuthUser(authentication);
+        UUID userId = oauthUserProcessor.fetchUserIdByOAuthUser(oauthUser);
 
-        UUID volunteerId = volunteerQueryUseCase.getVolunteerIdByOAuthId(oAuthId);
-        EncodedToken accessToken =
-                generateTokensOnLoginUseCase.saveRefreshTokenAndReturnAccessToken(
-                        volunteerId, UserRole.VOLUNTEER
-                );
-
-        cookieUseCase.setAccessToken(response, accessToken.value());
+        processAccessToken(response, userId);
         redirectUseCase.redirect(request, response, frontendRootUrl);
     }
 
-    private static OAuthProvider getOAuthProvider(Authentication authentication) {
+    private void processAccessToken(HttpServletResponse response, UUID userId) {
+        EncodedToken accessToken =
+                generateTokensOnLoginUseCase.saveRefreshTokenAndReturnAccessToken(
+                        userId, UserRole.VOLUNTEER);
+
+        setAccessToken(response, accessToken);
+    }
+
+    private void setAccessToken(HttpServletResponse response, EncodedToken accessToken) {
+        response.addHeader(AUTHORIZATION, accessToken.getValueWithPrefix());
+    }
+
+    private OAuth2User extractOAuthUser(Authentication authentication) {
         if (authentication instanceof OAuth2AuthenticationToken token) {
-            return OAuthProvider.from(token.getAuthorizedClientRegistrationId());
+            return token.getPrincipal();
         }
-        throw new IllegalArgumentException();
+        log.error("Authentication 객체가 OAuth2AuthenticationToken 타입이 아닙니다: {}", authentication.getClass().getName());
+        throw new IllegalArgumentException("잘못된 인증 객체입니다.");
     }
 }
