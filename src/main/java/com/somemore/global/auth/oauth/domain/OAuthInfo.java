6afd89a0c2fc9903c81a7cd3324ba57653fc1011
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-30 14:50:17 +0900

    feat(oauth): oauth 정보 도메인(엔티티) 추가

---
+++ src/main/java/com/somemore/global/auth/oauth/domain/OAuthInfo.java
@@ -0,0 +1,45 @@
+package com.somemore.global.auth.oauth.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "oauth_info")
+public class OAuthInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "oauth_id", nullable = false)
+    private String oauthId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "oauth_provider", nullable = false)
+    private OAuthProvider oAuthProvider;
+
+    @Builder
+    public OAuthInfo(UUID userId, String oauthId, OAuthProvider oAuthProvider) {
+        this.userId = userId;
+        this.oauthId = oauthId;
+        this.oAuthProvider = oAuthProvider;
+    }
+}
