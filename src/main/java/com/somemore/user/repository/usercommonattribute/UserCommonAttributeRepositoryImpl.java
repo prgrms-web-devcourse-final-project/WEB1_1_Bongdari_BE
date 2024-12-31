73a1d13602a236ee4d9146bc11a5b62174ebb593
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-31 16:37:31 +0900

    feat(user): 유저 공통 속성 리포지토리 조회 기능 추가

--- src/main/java/com/somemore/user/repository/usercommonattribute/UserCommonAttributeRepositoryImpl.java
+++ src/main/java/com/somemore/user/repository/usercommonattribute/UserCommonAttributeRepositoryImpl.java
@@ -1,14 +1,29 @@
 package com.somemore.user.repository.usercommonattribute;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.somemore.user.domain.QUserCommonAttribute;
 import com.somemore.user.domain.UserCommonAttribute;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class UserCommonAttributeRepositoryImpl implements UserCommonAttributeRepository {
 
+    private final JPAQueryFactory queryFactory;
     private final UserCommonAttributeJpaRepository userCommonAttributeJpaRepository;
+
+    private static final QUserCommonAttribute userCommonAttribute = QUserCommonAttribute.userCommonAttribute;
+
+    @Override
+    public Optional<UserCommonAttribute> findByUserId(UUID userId) {
+        return Optional.ofNullable(
+                queryFactory.selectFrom(userCommonAttribute)
+                        .fetchOne());
+    }
 
     @Override
     public UserCommonAttribute save(UserCommonAttribute userCommonAttribute) {
