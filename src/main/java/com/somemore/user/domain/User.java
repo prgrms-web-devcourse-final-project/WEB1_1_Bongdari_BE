7750e80f12ea75ed3d7160ccc67313eefeaf12cc
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-31 21:49:19 +0900

    refactor(user): 유저 인증 정보 dto 네이밍 변경

--- src/main/java/com/somemore/user/domain/User.java
+++ src/main/java/com/somemore/user/domain/User.java
@@ -1,7 +1,7 @@
 package com.somemore.user.domain;
 
 import com.somemore.global.common.entity.BaseEntity;
-import com.somemore.user.dto.UserAuthInfo;
+import com.somemore.user.dto.UserAuthInfoRequestDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -39,10 +39,10 @@ public class User extends BaseEntity {
     private UserRole role;
 
 
-    public static User from(UserAuthInfo userAuthInfo, UserRole role) {
+    public static User from(UserAuthInfoRequestDto userAuthInfoRequestDto, UserRole role) {
         return User.builder()
-                .email(userAuthInfo.email())
-                .password(userAuthInfo.password())
+                .email(userAuthInfoRequestDto.email())
+                .password(userAuthInfoRequestDto.password())
                 .role(role)
                 .build();
     }
--- src/main/java/com/somemore/user/dto/UserAuthInfoRequestDto.java
+++ src/main/java/com/somemore/user/dto/UserAuthInfoRequestDto.java
@@ -4,12 +4,12 @@ import com.somemore.global.auth.oauth.domain.OAuthProvider;
 
 import java.util.UUID;
 
-public record UserAuthInfo(String email,
-                           String password) {
+public record UserAuthInfoRequestDto(String email,
+                                     String password) {
 
-    public static UserAuthInfo createForOAuth(OAuthProvider provider) {
+    public static UserAuthInfoRequestDto createForOAuth(OAuthProvider provider) {
         String email = provider.getProviderName() + UUID.randomUUID();
         String password = String.valueOf(UUID.randomUUID());
-        return new UserAuthInfo(email, password);
+        return new UserAuthInfoRequestDto(email, password);
     }
 }
