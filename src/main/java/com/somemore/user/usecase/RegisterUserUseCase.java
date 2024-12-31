cdf7441ded2ad7c8a28324165072017671641d94
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-31 21:49:19 +0900

    refactor(user): 유저 인증 정보 dto 네이밍 변경

--- src/main/java/com/somemore/user/usecase/RegisterUserUseCase.java
+++ src/main/java/com/somemore/user/usecase/RegisterUserUseCase.java
@@ -3,9 +3,9 @@ package com.somemore.user.usecase;
 import com.somemore.user.domain.UserRole;
 import com.somemore.global.auth.oauth.domain.CommonOAuthInfo;
 import com.somemore.user.domain.User;
-import com.somemore.user.dto.UserAuthInfo;
+import com.somemore.user.dto.UserAuthInfoRequestDto;
 
 public interface RegisterUserUseCase {
     User registerOAuthUser(CommonOAuthInfo commonOAuthInfo, UserRole role);
-    User registerLocalUser(UserAuthInfo userAuthInfo, UserRole role);
+    User registerLocalUser(UserAuthInfoRequestDto userAuthInfoRequestDto, UserRole role);
 }
