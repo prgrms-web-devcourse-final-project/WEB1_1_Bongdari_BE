1cb824909eed692c63d470db27c637a15fd4b45c
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-31 16:31:43 +0900

    feat(user): 유저 리포지토리 조회 기능 추가

--- src/main/java/com/somemore/user/repository/user/UserRepository.java
+++ src/main/java/com/somemore/user/repository/user/UserRepository.java
@@ -2,7 +2,11 @@ package com.somemore.user.repository.user;
 
 import com.somemore.user.domain.User;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserRepository {
 
+    Optional<User> findById(UUID id);
     User save(User user);
 }
--- src/main/java/com/somemore/user/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/somemore/user/repository/user/UserRepositoryImpl.java
@@ -1,14 +1,34 @@
 package com.somemore.user.repository.user;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.somemore.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
+    private final JPAQueryFactory queryFactory;
     private final UserJpaRepository userJpaRepository;
+
+    private static final com.somemore.user.domain.QUser user = com.somemore.user.domain.QUser.user;
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return Optional.ofNullable(
+                queryFactory.selectFrom(user)
+                        .where(isNotDeleted())
+                        .fetchOne());
+    }
+
+    private static BooleanExpression isNotDeleted() {
+        return user.deleted.eq(false);
+    }
 
     @Override
     public User save(User user) {
