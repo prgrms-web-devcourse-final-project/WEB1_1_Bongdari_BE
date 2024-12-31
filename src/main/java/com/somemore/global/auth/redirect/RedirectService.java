70f19b4770a5b6463ef5911b25de05a17e891a97
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-12-31 14:40:33 +0900

    feat(redirection): 로그인 리디렉션 수정
    - 로직 개선
    - 실패 케이스 추가

--- src/main/java/com/somemore/global/auth/redirect/RedirectService.java
+++ src/main/java/com/somemore/global/auth/redirect/RedirectService.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +15,33 @@ import java.io.IOException;
 @Slf4j
 public class RedirectService implements RedirectUseCase {
 
+    @Value("${app.front-url}")
+    private String frontUrl;
+
     private final RedirectStrategy redirectStrategy;
 
+    private static final String ERROR_PATH = "/error";
+
     @Override
-    public void redirect(HttpServletRequest request, HttpServletResponse response, String url) throws IOException {
-        redirectStrategy.sendRedirect(request, response, url);
+    public void redirect(HttpServletRequest request, HttpServletResponse response, String path) {
+        try {
+            redirectStrategy.sendRedirect(request, response, path);
+        } catch (IOException e) {
+            log.error("리디렉션 중 오류 발생 - 대상 URL: {}, 메시지: {}", path, e.getMessage());
+            handleRedirectFailure(request, response);
+        }
+    }
+
+    private void handleRedirectFailure(HttpServletRequest request, HttpServletResponse response) {
+        String fallbackUrl = buildFallbackUrl();
+        try {
+            redirectStrategy.sendRedirect(request, response, fallbackUrl);
+        } catch (IOException e) {
+            log.error("에러 페이지로 리디렉션 시도 중 오류 발생 - 메시지: {}", e.getMessage());
+        }
+    }
+
+    private String buildFallbackUrl() {
+        return frontUrl + RedirectService.ERROR_PATH;
     }
 }
