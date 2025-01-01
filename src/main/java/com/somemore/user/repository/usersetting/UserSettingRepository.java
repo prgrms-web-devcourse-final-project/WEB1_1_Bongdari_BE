3936954a3bf1fc4fccf9fd22eadacee66726b1d8
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-02 06:15:48 +0900

    feat(user): 유저 세팅 조회 기능 추가
    - 유저 아이디 기준 조회.

--- src/main/java/com/somemore/user/repository/usersetting/UserSettingRepository.java
+++ src/main/java/com/somemore/user/repository/usersetting/UserSettingRepository.java
@@ -2,7 +2,11 @@ package com.somemore.user.repository.usersetting;
 
 import com.somemore.user.domain.UserSetting;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserSettingRepository {
 
+    Optional<UserSetting> findByUserId(UUID userId);
     UserSetting save(UserSetting userSetting);
 }
--- src/main/java/com/somemore/user/repository/usersetting/UserSettingRepositoryImpl.java
+++ src/main/java/com/somemore/user/repository/usersetting/UserSettingRepositoryImpl.java
@@ -1,14 +1,34 @@
 package com.somemore.user.repository.usersetting;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.somemore.user.domain.QUserSetting;
 import com.somemore.user.domain.UserSetting;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class UserSettingRepositoryImpl implements UserSettingRepository {
 
+    private final JPAQueryFactory queryFactory;
     private final UserSettingJpaRepository userSettingJpaRepository;
+
+    private static final QUserSetting userSetting = QUserSetting.userSetting;
+
+    @Override
+    public Optional<UserSetting> findByUserId(UUID userId) {
+        return Optional.ofNullable(
+                queryFactory.selectFrom(userSetting)
+                        .where(
+                                userSetting.userId.eq(userId),
+                                userSetting.deleted.eq(false)
+                        )
+                        .fetchOne()
+        );
+    }
 
     @Override
     public UserSetting save(UserSetting userSetting) {
