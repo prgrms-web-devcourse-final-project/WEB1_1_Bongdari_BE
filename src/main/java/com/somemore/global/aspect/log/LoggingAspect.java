7c3deae49d113a3cfad588734a652cc7dca5d08d
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2025-01-06 16:00:30 +0900

    Refactor/252 Logging 설정 추가및 리팩토링 (#270)
    * chore: 로그 파일 디렉토리를 gitignore 추가
    
    -root/logs 디렉토리의 ignore 설정
    
    * feat: loki 의존성 추가
    
    - 로그 저장, 시각화를 위한 loki 의존성 추가
    
    * refactor: logback-spring.xml 리팩토링
    
    - loki 관련 설정 추가
    - 로그 패턴의 단일화 적용
    - 파일 저장 appender의 설정 변경
    
    * chore: jacoco 예외 클래스 추가
    
    - Extractor 클래스의 테스트 커버리지 예외 조건 추가
    
    * refactor: logging AOP 리팩토링
    
    - 로그 기록 세분화
    - 민감한 데이터 마스킹 처리
    - 책임에 따른 클래스 분리
    - 마스킹 테스트및 검증 완
    
    * feat: log 공통 응답 객체 생성
    
    - 로그 응답에 대한 Response 객체 생성
    
    * refactor: 명시적인 생성자 제거및 어노테이션 대체
    
    - ParameterExtractor의 생성자 코드 제거및 RequiredArgs 어노테이션 추가
    - ResponseExtractor의 생성자 코드 제거및 RequiredArgs 어노테이션 추가
    - SensitiveDataMasker의 불필요한 어노테이션 제거
    
    * chore: 잠재적 에러 예방을 위한 개행 추가
    
    - logback-spring.xml 파일의 마지막 라인에 개행 추가

---
+++ src/main/java/com/somemore/global/aspect/log/LoggingAspect.java
@@ -0,0 +1,90 @@
+package com.somemore.global.aspect.log;
+
+import com.somemore.global.aspect.log.extractor.ParameterExtractor;
+import com.somemore.global.aspect.log.extractor.RequestExtractor;
+import com.somemore.global.aspect.log.extractor.ResponseExtractor;
+import com.somemore.global.common.response.LoggedResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private final RequestExtractor requestExtractor;
+    private final ResponseExtractor responseExtractor;
+    private final ParameterExtractor parameterExtractor;
+
+    @Pointcut("execution(* com.somemore.domains.*.controller..*.*(..))")
+    private void controllerPointCut() {}
+
+    @Around("controllerPointCut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String requestId = UUID.randomUUID().toString();
+        MDC.put("requestId", requestId);
+
+        try {
+            return doLogAround(joinPoint);
+        } finally {
+            MDC.remove("requestId");
+        }
+    }
+
+    private Object doLogAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().toShortString();
+        HttpServletRequest request = requestExtractor.getCurrentRequest();
+
+        MDC.put("method", request.getMethod());
+        MDC.put("uri", request.getRequestURI());
+
+        String params = parameterExtractor.extractParameters(joinPoint);
+        log.info("엔드포인트 호출: {} \n- URI: {} \n- Method: {} \n- 파라미터: {}",
+                methodName,
+                request.getRequestURI(),
+                request.getMethod(),
+                params);
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            Object result = joinPoint.proceed();
+            long elapsedTime = System.currentTimeMillis() - startTime;
+
+            LoggedResponse loggedResponse = responseExtractor.extractResponse(result);
+            log.info("호출 성공: {} \n- 응답 코드: {} \n- 응답 값: {} \n- 실행 시간: {}ms",
+                    methodName,
+                    loggedResponse.getStatusCode(),
+                    loggedResponse.getBody(),
+                    elapsedTime);
+
+            return result;
+        } catch (Exception e) {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            HttpStatus status = responseExtractor.extractExceptionStatus(e);
+
+            log.warn("예외 발생: {} \n- 예외 코드: {} \n- 예외 타입: {} \n- 예외 메세지: {} \n- 실행 시간: {}ms",
+                    methodName,
+                    status,
+                    e.getClass().getSimpleName(),
+                    e.getMessage(),
+                    elapsedTime);
+
+            throw e;
+        } finally {
+            MDC.clear();
+        }
+    }
+}
