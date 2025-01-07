4f876730d7cd27e4adeaa49d14b4d804dca57eb7
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-07 23:09:08 +0900

    feat(refreshToken): 유저 아이디 기준 리프레시 토큰 조회 기능 추가

--- src/main/java/com/somemore/global/auth/jwt/refresh/manager/RefreshTokenManager.java
+++ src/main/java/com/somemore/global/auth/jwt/refresh/manager/RefreshTokenManager.java
@@ -3,8 +3,12 @@ package com.somemore.global.auth.jwt.refresh.manager;
 import com.somemore.global.auth.jwt.domain.EncodedToken;
 import com.somemore.global.auth.jwt.refresh.domain.RefreshToken;
 
+import java.util.UUID;
+
 public interface RefreshTokenManager {
-    RefreshToken findRefreshToken(EncodedToken accessToken);
+    RefreshToken findRefreshTokenByAccessToken(EncodedToken accessToken);
+
+    RefreshToken findRefreshTokenByUserId(UUID userId);
 
     void save(RefreshToken refreshToken);
 
