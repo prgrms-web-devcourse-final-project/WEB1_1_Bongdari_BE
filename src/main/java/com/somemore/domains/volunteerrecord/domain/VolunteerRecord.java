fbcd8ac4f2204fa810169ca3866fd44f1f197503
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2025-01-10 16:44:25 +0900

    feat: 봉사 기록 엔티티 추가
    - 봉사 기록 엔티티 추가
    - 엔티티 생성 메서드 구현

---
+++ src/main/java/com/somemore/domains/volunteerrecord/domain/VolunteerRecord.java
@@ -0,0 +1,54 @@
+package com.somemore.domains.volunteerrecord.domain;
+
+import com.somemore.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "volunteer_record")
+public class VolunteerRecord extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "volunteer_id", nullable = false)
+    private UUID volunteerId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "volunteer_date", nullable = false)
+    private LocalDate volunteerDate;
+
+    @Column(name = "volunteer_hours", nullable = false)
+    private int volunteerHours;
+
+    @Builder
+    private VolunteerRecord(UUID volunteerId, String title, LocalDate volunteerDate, int volunteerHours) {
+        this.volunteerId = volunteerId;
+        this.title = title;
+        this.volunteerDate = volunteerDate;
+        this.volunteerHours = volunteerHours;
+    }
+
+    public static VolunteerRecord create(UUID volunteerId, String title, LocalDate volunteerDate, int volunteerHours) {
+        return VolunteerRecord.builder()
+                .volunteerId(volunteerId)
+                .title(title)
+                .volunteerDate(volunteerDate)
+                .volunteerHours(volunteerHours)
+                .build();
+    }
+
+}
