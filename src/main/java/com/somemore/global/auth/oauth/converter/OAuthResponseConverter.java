fc9a7f6db7511fa7f45fe73153ec81c424f23267
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-12 23:44:04 +0900

    feat(oauth): CustomOAuth2User 사용으로 로직 추가, 변경
    - 제공자 정보를 제공 받음으로 올바른 분기 처리.
    - OAuth2User 사용처에서 CustomOAuth2User 사용하도록 변경.

--- src/main/java/com/somemore/global/auth/oauth/converter/OAuthResponseConverter.java
+++ src/main/java/com/somemore/global/auth/oauth/converter/OAuthResponseConverter.java
@@ -1,8 +1,8 @@
 package com.somemore.global.auth.oauth.converter;
 
 import com.somemore.global.auth.oauth.domain.CommonOAuthInfo;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import com.somemore.global.auth.oauth.domain.CustomOAuth2User;
 
 public interface OAuthResponseConverter {
-    CommonOAuthInfo convert(OAuth2User oAuth2User);
+    CommonOAuthInfo convert(CustomOAuth2User oAuth2User);
 }
