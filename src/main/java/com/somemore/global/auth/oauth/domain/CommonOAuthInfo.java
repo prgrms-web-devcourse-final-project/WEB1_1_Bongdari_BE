8a1adfb018bbf6f3e7a885db0cf6e129f4ec1e42
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-12 23:40:36 +0900

    feat(oauth): 공통 oauth 정보 클래스 정적 팩토리 메서드 추가

--- src/main/java/com/somemore/global/auth/oauth/domain/CommonOAuthInfo.java
+++ src/main/java/com/somemore/global/auth/oauth/domain/CommonOAuthInfo.java
@@ -3,4 +3,8 @@ package com.somemore.global.auth.oauth.domain;
 public record CommonOAuthInfo(
         OAuthProvider provider,
         String oauthId) {
+
+    public static CommonOAuthInfo of(OAuthProvider provider, String oauthId) {
+        return new CommonOAuthInfo(provider, oauthId);
+    }
 }
