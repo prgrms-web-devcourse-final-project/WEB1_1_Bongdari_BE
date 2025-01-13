bee71b7d9ca15768f223b273c61aa9e957b3e251
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-13 20:07:18 +0900

    feat(cookie): 쿠키 관련 로직 삭제

--- src/main/java/com/somemore/global/auth/signout/usecase/SignOutUseCase.java
+++ src/main/java/com/somemore/global/auth/signout/usecase/SignOutUseCase.java
@@ -2,7 +2,9 @@ package com.somemore.global.auth.signout.usecase;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.UUID;
+
 public interface SignOutUseCase {
 
-    void signOut(HttpServletResponse response, String volunteerId);
+    void signOut(HttpServletResponse response, UUID userId);
 }
