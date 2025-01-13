53a14196bf4c493ac2432dc3d5706489b1326853
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-13 21:00:48 +0900

    feat(ApiResponse): 가독성 개선, 메서드 추가

--- src/main/java/com/somemore/global/common/response/ApiResponse.java
+++ src/main/java/com/somemore/global/common/response/ApiResponse.java
@@ -3,10 +3,13 @@ package com.somemore.global.common.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @NoArgsConstructor
 @Getter
 public class ApiResponse<T> {
+
+    public static final String EMPTY = "";
 
     @Schema(description = "응답 상태 코드. 성공 시 200, 실패 시 오류 코드", example = "200")
     private int code;
@@ -15,16 +18,20 @@ public class ApiResponse<T> {
     @Schema(description = "API 요청 처리 결과로 반환되는 데이터")
     private T data;
 
-    public static <T> ApiResponse<T> ok(int status, T data, String message) {
-        return new ApiResponse<>(status, message, data);
+    public static ApiResponse<String> ok(String message) {
+        return new ApiResponse<>(HttpStatus.OK.value(), message, "");
+    }
+
+    public static <T> ApiResponse<T> ok(T data, String message) {
+        return new ApiResponse<>(HttpStatus.OK.value(), message, data);
     }
 
     public static ApiResponse<String> ok(int status, String message) {
-        return new ApiResponse<>(status, message, "");
+        return new ApiResponse<>(status, message, EMPTY);
     }
 
-    public static ApiResponse<String> ok(String message) {
-        return new ApiResponse<>(200, message, "");
+    public static <T> ApiResponse<T> ok(int status, T data, String message) {
+        return new ApiResponse<>(status, message, data);
     }
 
     public ApiResponse(int code, String message, T data) {
