f7a79b6a0352f5544613ff097a4b120b20c4d6d5
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-13 21:02:54 +0900

    refactor: DTO 명명 구체화

--- src/main/java/com/somemore/global/auth/sign/in/SignInRequestDto.java
+++ src/main/java/com/somemore/global/auth/sign/in/SignInRequestDto.java
@@ -1,11 +1,11 @@
-package com.somemore.global.auth.dto;
+package com.somemore.global.auth.sign.in;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonNaming(SnakeCaseStrategy.class)
-public record SignRequestDto(
+public record SignInRequestDto(
         @Schema(description = "ID", example = "somemore")
         String accountId,
         @Schema(description = "PW", example = "password1234")
