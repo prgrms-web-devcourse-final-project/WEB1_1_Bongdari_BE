7ea6c6c240cc711d4214538d668e2b48f5457737
leebs0521 <leebs0521@gmail.com>
2025-01-13 15:28:13 +0900

    test(recruit-board): RecruitBoard 수정 검증 리팩토링 테스트  - 봉사 시작/종료 일시 수정시, 생성일 하루 이후부터 가능하도록 검증

--- src/test/java/com/somemore/domains/recruitboard/service/validator/RecruitBoardValidatorTest.java
+++ src/test/java/com/somemore/domains/recruitboard/service/validator/RecruitBoardValidatorTest.java
@@ -1,20 +1,20 @@
 package com.somemore.domains.recruitboard.service.validator;
 
+import static com.somemore.global.exception.ExceptionMessage.INVALID_RECRUIT_BOARD_TIME;
+import static com.somemore.global.exception.ExceptionMessage.INVALID_RECRUIT_BOARD_TIME_UPDATE;
+import static com.somemore.global.exception.ExceptionMessage.UNAUTHORIZED_RECRUIT_BOARD;
+import static com.somemore.support.fixture.RecruitBoardFixture.createRecruitBoard;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import com.somemore.domains.recruitboard.domain.RecruitBoard;
 import com.somemore.global.exception.BadRequestException;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import static com.somemore.global.exception.ExceptionMessage.INVALID_RECRUIT_BOARD_TIME;
-import static com.somemore.global.exception.ExceptionMessage.UNAUTHORIZED_RECRUIT_BOARD;
-import static com.somemore.support.fixture.LocalDateTimeFixture.createStartDateTime;
-import static com.somemore.support.fixture.RecruitBoardFixture.createRecruitBoard;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RecruitBoardValidatorTest {
 
@@ -23,16 +23,47 @@ class RecruitBoardValidatorTest {
     @DisplayName("봉사 종료 시간이 시작 시간과 같거나 빠르면, 봉사 모집글 생성 시 에러가 발생한다")
     @ParameterizedTest
     @ValueSource(longs = {0, -1})
-    void createRecruitBoardWithInValidVolunteerTime(long minutesOffset) {
+    void validateRecruitBoardTimeWhenNotValid(long minutesOffset) {
         // given
-        LocalDateTime now = createStartDateTime();
+        LocalDateTime now = LocalDateTime.of(2025, 1, 1, 10, 0);
         LocalDateTime endDateTime = now.plusMinutes(minutesOffset);
 
-        // when & then
+        // when
+        // then
         assertThatThrownBy(
                 () -> validator.validateRecruitBoardTime(now, endDateTime))
                 .isInstanceOf(BadRequestException.class)
                 .hasMessage(INVALID_RECRUIT_BOARD_TIME.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("StartDateTime이 createdAt + 1일 이후일 경우 예외 없이 통과")
+    void validateUpdateRecruitBoardTime() {
+        // given
+        LocalDateTime createdAt = LocalDateTime.of(2025, 1, 1, 10, 0); // 2025-01-01 10:00:00
+        LocalDateTime start = LocalDateTime.of(2025, 1, 2, 10, 0); // 2025-01-02 10:00:00
+        LocalDateTime end = LocalDateTime.of(2025, 1, 3, 10, 0); // 2025-01-03 10:00:00
+
+        // when
+        // then
+        assertDoesNotThrow(() -> validator.validateUpdateRecruitBoardTime(createdAt, start, end));
+    }
+
+    @Test
+    @DisplayName("StartDateTime이 createdAt + 1일 이전일 경우 예외 발생")
+    void validateUpdateRecruitBoardTimeWhenNotValid() {
+        // given
+        LocalDateTime createdAt = LocalDateTime.of(2025, 1, 2, 10, 0); // 2025-1-1 10:00:00
+        LocalDateTime start = LocalDateTime.of(2025, 1, 2, 12, 0); // 2025-1-2 12:00:00
+        LocalDateTime end = LocalDateTime.of(2025, 1, 2, 14, 0); // 2025-1-2 14:00:00
+
+        // when
+        // then
+        assertThatThrownBy(
+                () -> validator.validateUpdateRecruitBoardTime(createdAt, start, end))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage(INVALID_RECRUIT_BOARD_TIME_UPDATE.getMessage());
 
     }
 
