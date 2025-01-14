97c9e20a394b7dda3bd88c427445f45a8ef623b5
leebs0521 <leebs0521@gmail.com>
2025-01-14 17:27:43 +0900

    refactor(recruit-board): 스케쥴링 작업 중 에러 발생시 로깅 처리

--- src/main/java/com/somemore/domains/recruitboard/scheduler/RecruitBoardStatusUpdateScheduler.java
+++ src/main/java/com/somemore/domains/recruitboard/scheduler/RecruitBoardStatusUpdateScheduler.java
@@ -30,10 +30,14 @@ public class RecruitBoardStatusUpdateScheduler {
         LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
         LocalDateTime startOfNextDay = LocalDate.now().plusDays(1).atStartOfDay();
 
-        long updatedCount = recruitBoardRepository.updateRecruitingToClosedByStartDate(startOfDay,
-                startOfNextDay);
-        log.info("총 {}개의 모집글 상태를 CLOSED로 변경 완료", updatedCount);
-
+        try {
+            long updatedCount = recruitBoardRepository.updateRecruitingToClosedByStartDate(
+                    startOfDay, startOfNextDay);
+            log.info("총 {}개의 모집글 상태를 CLOSED로 변경 완료", updatedCount);
+        } catch (Exception e) {
+            log.error("봉사 시작일에 해당하는 모집글 상태를 CLOSED로 변경하는 중 오류 발생", e);
+            throw e;
+        }
     }
 
     @Retryable(
@@ -46,9 +50,13 @@ public class RecruitBoardStatusUpdateScheduler {
         log.info("봉사 종료일이 지난 모집글 상태를 COMPLETED로 변경하는 작업 시작");
         LocalDateTime now = LocalDateTime.now();
 
-        long updatedCount = recruitBoardRepository.updateClosedToCompletedByEndDate(now);
-        log.info("총 {}개의 모집글 상태를 COMPLETED로 변경 완료", updatedCount);
-
+        try {
+            long updatedCount = recruitBoardRepository.updateClosedToCompletedByEndDate(now);
+            log.info("총 {}개의 모집글 상태를 COMPLETED로 변경 완료", updatedCount);
+        } catch (Exception e) {
+            log.error("봉사 종료일이 지난 모집글 상태를 COMPLETED로 변경하는 중 오류 발생", e);
+            throw e;
+        }
     }
 
 }
