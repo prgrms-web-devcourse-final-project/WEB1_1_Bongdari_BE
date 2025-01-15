698d84406b04e2de9bc0864958f7fabd2212c8c6
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-15 15:35:30 +0900

    feat(register): 회원 가입 로직 (유저가 생성된 후, 봉사자나 기관이 생성)

---
+++ src/main/java/com/somemore/global/auth/sign/up/SignUpService.java
@@ -0,0 +1,46 @@
+package com.somemore.global.auth.sign.up;
+
+import com.somemore.center.usecase.RegisterCenterUseCase;
+import com.somemore.user.domain.User;
+import com.somemore.user.domain.UserRole;
+import com.somemore.user.dto.UserAuthInfo;
+import com.somemore.user.service.RegisterUserUseCase;
+import com.somemore.volunteer.usecase.RegisterVolunteerUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SignUpService implements SignUpUseCase {
+
+    private final RegisterUserUseCase registerUserUseCase;
+    private final RegisterVolunteerUseCase registerVolunteerUseCase;
+    private final RegisterCenterUseCase registerCenterUseCase;
+
+    @Override
+    public void signUp(SignUpRequestDto signUpRequestDto) {
+        User user = registerUserUseCase.registerLocalUser(
+                UserAuthInfo.of(signUpRequestDto.accountId(), signUpRequestDto.accountPassword()),
+                UserRole.from(signUpRequestDto.userRole()));
+
+        // TODO 회원가입 이벤트 발행으로 변경 (봉사자 혹은 기관 등록)
+        // 임시 로직
+        registerVolunteerOrCenter(user);
+    }
+
+    private void registerVolunteerOrCenter(User user) {
+        UUID userId = user.getId();
+        UserRole role = user.getRole();
+
+        if (role == UserRole.VOLUNTEER) {
+            registerVolunteerUseCase.register(userId);
+            return;
+        }
+
+        registerCenterUseCase.register(userId);
+    }
+}
