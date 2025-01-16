71b5df0ce3d033f84a1d2d11c8fc4ab99c81a5d2
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-16 16:14:13 +0900

    fix: center import

--- src/main/java/com/somemore/domains/center/domain/Center.java
+++ src/main/java/com/somemore/domains/center/domain/Center.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
