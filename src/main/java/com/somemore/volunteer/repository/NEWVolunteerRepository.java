7d552c75aa016579362d550ccf408d09b0e3daff
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-16 14:35:14 +0900

    feat(volunteer): 봉사자 아이디, 유저 아이디 기준 조회 기능 추가

--- src/main/java/com/somemore/volunteer/repository/NEWVolunteerRepository.java
+++ src/main/java/com/somemore/volunteer/repository/NEWVolunteerRepository.java
@@ -2,7 +2,14 @@ package com.somemore.volunteer.repository;
 
 import com.somemore.volunteer.domain.NEWVolunteer;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface NEWVolunteerRepository {
 
     NEWVolunteer save(NEWVolunteer volunteer);
+
+    Optional<NEWVolunteer> findById(UUID id);
+
+    Optional<NEWVolunteer> findByUserId(UUID userId);
 }
