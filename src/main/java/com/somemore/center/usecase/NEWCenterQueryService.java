b7e5b22471d229316036ac5783e04e67a4fd72b3
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-19 22:37:26 +0900

    feat(user): userId 기준 roleId 조회 기능 추가

---
+++ src/main/java/com/somemore/center/usecase/NEWCenterQueryService.java
@@ -0,0 +1,30 @@
+package com.somemore.center.usecase;
+
+import com.somemore.center.domain.NEWCenter;
+import com.somemore.center.repository.NEWCenterRepository;
+import com.somemore.global.exception.ExceptionMessage;
+import com.somemore.global.exception.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NEWCenterQueryService implements NEWCenterQueryUseCase {
+
+    private final NEWCenterRepository centerRepository;
+
+    @Override
+    public NEWCenter getByUserId(UUID userId) {
+        return centerRepository.findByUserId(userId)
+                .orElseThrow(() -> new NoSuchElementException(ExceptionMessage.NOT_EXISTS_CENTER));
+    }
+
+    @Override
+    public UUID getIdByUserId(UUID userId) {
+        return getByUserId(userId).getId();
+    }
+}
