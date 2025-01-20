e680a0f092d57b5aaec600064d7133f75c9010a5
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-20 16:49:55 +0900

    feat(annotation): userId, roleId 추출 어노테이션 추가

---
+++ src/main/java/com/somemore/global/auth/annotation/RoleIdArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.somemore.global.auth.annotation;
+
+import com.somemore.global.auth.authentication.UserIdentity;
+import com.somemore.global.exception.InvalidAuthenticationException;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static com.somemore.global.exception.ExceptionMessage.AUTHENTICATION_MISSING;
+import static com.somemore.global.exception.ExceptionMessage.INVALID_PRINCIPAL_TYPE;
+
+@Component
+public class RoleIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(RoleId.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            throw new InvalidAuthenticationException(AUTHENTICATION_MISSING);
+        }
+
+        if (authentication.getPrincipal() instanceof UserIdentity principal) {
+            return principal.roleId();
+        }
+
+        throw new InvalidAuthenticationException(INVALID_PRINCIPAL_TYPE);
+    }
+}
