ce0512ef93f66558136e4489364cdae79fd254ff
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-21 21:12:30 +0900

    feat: BCrypt 해시 유틸 추가

---
+++ src/main/java/com/somemore/global/encoder/BCryptPasswordEncoderUtil.java
@@ -0,0 +1,21 @@
+package com.somemore.global.encoder;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class BCryptPasswordEncoderUtil {
+
+    private static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+
+    private BCryptPasswordEncoderUtil() {
+        throw new UnsupportedOperationException("유틸리티 클래스는 인스턴스화할 수 없습니다.");
+    }
+
+    public static String encode(String rawPassword) {
+        return PASSWORD_ENCODER.encode(rawPassword);
+    }
+
+    public static boolean matches(String rawPassword, String encodedPassword) {
+        return PASSWORD_ENCODER.matches(rawPassword, encodedPassword);
+    }
+}
