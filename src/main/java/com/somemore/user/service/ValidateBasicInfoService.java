b0731c16c0908680fc0dba133dac9fbaa7da72bb
조재중 <9gle3492@gmail.com>
2025-01-23 16:55:28 +0900

    feat(user): 유저 기본 정보 입력 상태 확인 API 추가 (#333)
    - `/check/basic-info` 엔드포인트 추가
    - UUID 기반으로 유저의 필수 입력 정보 완료 여부 확인

--- src/main/java/com/somemore/user/service/ValidateBasicInfoService.java
+++ src/main/java/com/somemore/user/service/ValidateBasicInfoService.java
@@ -1,6 +1,5 @@
 package com.somemore.user.service;
 
-import com.somemore.user.domain.UserCommonAttribute;
 import com.somemore.user.usecase.UserQueryUseCase;
 import com.somemore.user.usecase.ValidateBasicInfoUseCase;
 import lombok.RequiredArgsConstructor;
@@ -20,18 +19,6 @@ public class ValidateBasicInfoService implements ValidateBasicInfoUseCase {
 
     @Override
     public boolean isBasicInfoComplete(UUID userId) {
-        // 객체지향적: UserCommonAttribute 객체를 활용하여 데이터를 관리
-        // 최신 데이터와는 조금 멀어질 수 있음
-        UserCommonAttribute userCommonAttribute = userQueryUseCase.getCommonAttributeByUserId(userId);
-        boolean isCustomized1 = userCommonAttribute.isCustomized();
-
-        // 비용 절감: 특정 필드만 조회하여 데이터베이스 접근을 최소화
-        // 최신 데이터에 더 가까움
-        boolean isCustomized2 = userQueryUseCase.getIsCustomizedByUserId(userId);
-
-        // TODO:
-        // - 두 접근 방식 중 하나를 선택.
-
-        return isCustomized1 && isCustomized2;
+        return userQueryUseCase.getIsCustomizedByUserId(userId);
     }
 }
