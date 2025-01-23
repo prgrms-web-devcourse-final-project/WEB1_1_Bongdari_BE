7689b6c5095f93f21a16455e91993dd1ba79df79
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-23 16:46:04 +0900

    feat(user): 유저 기본 정보 입력 상태 확인 API 추가
    - `/check/basic-info` 엔드포인트 추가
    - UUID 기반으로 유저의 필수 입력 정보 완료 여부 확인

--- src/main/java/com/somemore/user/controller/UserQueryController.java
+++ src/main/java/com/somemore/user/controller/UserQueryController.java
@@ -1,7 +1,9 @@
 package com.somemore.user.controller;
 
+import com.somemore.global.auth.annotation.UserId;
 import com.somemore.global.common.response.ApiResponse;
 import com.somemore.user.usecase.UserQueryUseCase;
+import com.somemore.user.usecase.ValidateBasicInfoUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @Tag(name = "User Query API", description = "유저 조회 관련 API")
 @RestController
 @Slf4j
@@ -19,13 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserQueryController {
 
     private final UserQueryUseCase userQueryUseCase;
+    private final ValidateBasicInfoUseCase validateBasicInfoUseCase;
 
-    @GetMapping("/exists/")
+    @GetMapping("/exists")
     @Operation(summary = "아이디 중복 확인", description = "입력한 아이디가 중복되었는지 확인합니다.")
     public ApiResponse<Boolean> checkUserExists(
             @RequestParam String accountId
     ) {
         boolean isAccountIdDuplicate = userQueryUseCase.isDuplicateAccountId(accountId);
         return ApiResponse.ok(isAccountIdDuplicate, "중복 조회 완료");
+    }
+
+    @GetMapping("/check/basic-info")
+    @Operation(summary = "유저 기본 정보 입력 상태 확인", description = "현재 유저의 필수 입력 정보가 모두 완료되었는지 확인합니다."
+    )
+    public ApiResponse<Boolean> checkUserExists(
+            @UserId UUID userId
+    ) {
+        boolean isBasicInfoComplete = validateBasicInfoUseCase.isBasicInfoComplete(userId);
+        return ApiResponse.ok(isBasicInfoComplete, "필수 입력 정보 설정 조회 완료");
     }
 }
