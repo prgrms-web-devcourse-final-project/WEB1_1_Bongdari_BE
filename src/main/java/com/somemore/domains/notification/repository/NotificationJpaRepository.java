6062ac872181f458b6889c8623a4bd72b6f91bb8
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-03 14:58:18 +0900

    Revert "feat(notification): repository 계층에서 read, unread 구분하지 않기"
    This reverts commit 5fe6c1fa6ed92e980450df210a68d202edb7f991.

--- src/main/java/com/somemore/domains/notification/repository/NotificationJpaRepository.java
+++ src/main/java/com/somemore/domains/notification/repository/NotificationJpaRepository.java
@@ -3,9 +3,5 @@ package com.somemore.domains.notification.repository;
 import com.somemore.domains.notification.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.UUID;
-
 public interface NotificationJpaRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findAllByReceiverId(UUID receiverId);
 }
