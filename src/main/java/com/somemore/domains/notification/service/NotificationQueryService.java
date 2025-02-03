cf16309fdce9df8855b7efb279dbcd2f6419b18b
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-03 14:58:17 +0900

    Revert "test(notification): 알림 쿼리 서비스 세분화"
    This reverts commit e356b936dfba44f3c05d1cd121af24087b52ce6d.

--- src/main/java/com/somemore/domains/notification/service/NotificationQueryService.java
+++ src/main/java/com/somemore/domains/notification/service/NotificationQueryService.java
@@ -4,8 +4,6 @@ import com.somemore.domains.notification.domain.Notification;
 import com.somemore.domains.notification.dto.NotificationResponseDto;
 import com.somemore.domains.notification.repository.NotificationRepository;
 import com.somemore.domains.notification.usecase.NotificationQueryUseCase;
-import com.somemore.global.exception.ExceptionMessage;
-import com.somemore.global.exception.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.function.Predicate;
 
 @Slf4j
 @Service
@@ -24,34 +21,20 @@ public class NotificationQueryService implements NotificationQueryUseCase {
     private final NotificationRepository notificationRepository;
 
     @Override
-    public Notification getNotification(Long id) {
-        return notificationRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException(ExceptionMessage.NOT_EXISTS_NOTIFICATION));
-    }
-
-    @Override
-    public List<Notification> getNotifications(UUID userId) {
-        return notificationRepository.findAllByUserId(userId);
-    }
-
-    @Override
-    public List<Notification> getNotifications(List<Long> ids) {
-        return notificationRepository.findAllByIds(ids);
-    }
-
-    @Override
     public List<NotificationResponseDto> getUnreadNotifications(UUID userId) {
-        List<Notification> notifications = getNotifications(userId).stream()
-                .filter(Predicate.not(Notification::isRead))
-                .toList();
-        return NotificationResponseDto.from(notifications);
+        List<Notification> notifications = notificationRepository.findByReceiverIdAndUnread(userId);
+        return dtoFrom(notifications);
     }
 
     @Override
     public List<NotificationResponseDto> getReadNotifications(UUID userId) {
-        List<Notification> notifications = getNotifications(userId).stream()
-                .filter(Notification::isRead)
+        List<Notification> notifications = notificationRepository.findByReceiverIdAndRead(userId);
+        return dtoFrom(notifications);
+    }
+
+    private static List<NotificationResponseDto> dtoFrom(List<Notification> notifications) {
+        return notifications.stream()
+                .map(NotificationResponseDto::from)
                 .toList();
-        return NotificationResponseDto.from(notifications);
     }
 }
