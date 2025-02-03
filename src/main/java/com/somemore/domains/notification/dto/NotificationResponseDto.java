d56dbf668406725993909c31dfc4350e10df8241
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-03 14:58:18 +0900

    Revert "feat(notification): 복수형 정적 팩토리 메서드"
    This reverts commit 6dfe3fef531dd7624ea762a72f8599e5fe1062df.

--- src/main/java/com/somemore/domains/notification/dto/NotificationResponseDto.java
+++ src/main/java/com/somemore/domains/notification/dto/NotificationResponseDto.java
@@ -7,7 +7,6 @@ import com.somemore.domains.notification.domain.NotificationSubType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Schema(description = "알림 응답 DTO")
@@ -34,11 +33,5 @@ public record NotificationResponseDto(
                 notification.getRelatedId(),
                 notification.getCreatedAt()
         );
-    }
-
-    public static List<NotificationResponseDto> from(List<Notification> notifications) {
-        return notifications.stream()
-                .map(NotificationResponseDto::from)
-                .toList();
     }
 }
