eb2294107e955f13bfa70c8ac42a0fe7377770d6
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-03 14:50:09 +0900

    feat(notification): repository 계층에서 read, unread 구분하지 않기

--- src/main/java/com/somemore/domains/notification/repository/NotificationRepositoryImpl.java
+++ src/main/java/com/somemore/domains/notification/repository/NotificationRepositoryImpl.java
@@ -1,9 +1,6 @@
 package com.somemore.domains.notification.repository;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.somemore.domains.notification.domain.Notification;
-import com.somemore.domains.notification.domain.QNotification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -16,9 +13,6 @@ import java.util.UUID;
 public class NotificationRepositoryImpl implements NotificationRepository {
 
     private final NotificationJpaRepository notificationJpaRepository;
-    private final JPAQueryFactory queryFactory;
-
-    private static final QNotification notification = QNotification.notification;
 
     @Override
     public Notification save(Notification notification) {
@@ -36,35 +30,12 @@ public class NotificationRepositoryImpl implements NotificationRepository {
     }
 
     @Override
-    public List<Notification> findByReceiverIdAndUnread(UUID receiverId) {
-        return queryFactory.selectFrom(notification)
-                .where(eqReceiverId(receiverId),
-                        isUnread())
-                .fetch();
-    }
-
-    @Override
-    public List<Notification> findByReceiverIdAndRead(UUID receiverId) {
-        return queryFactory.selectFrom(notification)
-                .where(eqReceiverId(receiverId),
-                        isRead())
-                .fetch();
+    public List<Notification> findAllByUserId(UUID receiverId) {
+        return notificationJpaRepository.findAllByReceiverId(receiverId);
     }
 
     @Override
     public void deleteAllInBatch() {
         notificationJpaRepository.deleteAllInBatch();
-    }
-
-    private static BooleanExpression eqReceiverId(UUID userId) {
-        return notification.receiverId.eq(userId);
-    }
-
-    private static BooleanExpression isUnread() {
-        return notification.read.eq(false);
-    }
-
-    private static BooleanExpression isRead() {
-        return notification.read.eq(true);
     }
 }
