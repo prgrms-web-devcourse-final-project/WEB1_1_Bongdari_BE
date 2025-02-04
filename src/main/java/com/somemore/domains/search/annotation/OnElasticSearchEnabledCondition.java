df8c95e98ebb758db8496d25fa677b3d5471a495
ayoung-dev <dkdudab@hanmail.net>
2025-02-05 00:06:22 +0900

    refactor(search): 소나큐브 반영
    - 불필요한 import 제거
    - null 처리 추가

--- src/main/java/com/somemore/domains/search/annotation/OnElasticSearchEnabledCondition.java
+++ src/main/java/com/somemore/domains/search/annotation/OnElasticSearchEnabledCondition.java
@@ -3,18 +3,20 @@ package com.somemore.domains.search.annotation;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.MultiValueMap;
+
+import java.util.Objects;
 
 public class OnElasticSearchEnabledCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        MultiValueMap<String, Object> attrs = metadata.getAllAnnotationAttributes(
-                ConditionalOnElasticSearchEnabled.class.getName());
-        String propertyName = (String) attrs.getFirst("propertyName");
-        String havingValue = (String) attrs.getFirst("havingValue");
+        String propertyName = (String) Objects.requireNonNull(metadata.getAnnotationAttributes(
+                ConditionalOnElasticSearchEnabled.class.getName())).get("propertyName");
+        String havingValue = (String) Objects.requireNonNull(metadata.getAnnotationAttributes(
+                ConditionalOnElasticSearchEnabled.class.getName())).get("havingValue");
 
         String propertyValue = context.getEnvironment().getProperty(propertyName);
-        return havingValue.equals(propertyValue);
+
+        return havingValue.equals(propertyValue != null ? propertyValue : "");
     }
 }
