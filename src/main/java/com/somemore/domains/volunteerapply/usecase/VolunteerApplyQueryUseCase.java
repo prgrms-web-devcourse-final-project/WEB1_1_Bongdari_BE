2e3d8e2b0cf36d689510856317a415c66afc58b6
leebs0521 <leebs0521@gmail.com>
2025-02-04 12:14:42 +0900

    refactor(review): 리뷰 상세 조회시, 모집글 아이디 응답 필드 추가

--- src/main/java/com/somemore/domains/volunteerapply/usecase/VolunteerApplyQueryUseCase.java
+++ src/main/java/com/somemore/domains/volunteerapply/usecase/VolunteerApplyQueryUseCase.java
@@ -3,16 +3,17 @@ package com.somemore.domains.volunteerapply.usecase;
 import com.somemore.domains.volunteerapply.domain.VolunteerApply;
 import com.somemore.domains.volunteerapply.dto.condition.VolunteerApplySearchCondition;
 import com.somemore.domains.volunteerapply.dto.response.VolunteerApplySummaryResponseDto;
-import org.springframework.data.domain.Page;
-
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
 
 public interface VolunteerApplyQueryUseCase {
 
     VolunteerApply getById(Long id);
 
     VolunteerApply getByRecruitIdAndVolunteerId(Long recruitId, UUID volunteerId);
+
+    Long getRecruitBoardIdById(Long id);
 
     VolunteerApplySummaryResponseDto getSummaryByRecruitId(Long recruitId);
 
@@ -22,6 +23,6 @@ public interface VolunteerApplyQueryUseCase {
 
     Page<VolunteerApply> getAllByRecruitId(Long recruitId, VolunteerApplySearchCondition condition);
 
-    Page<VolunteerApply> getAllByVolunteerId(UUID volunteerId, VolunteerApplySearchCondition condition);
-
+    Page<VolunteerApply> getAllByVolunteerId(UUID volunteerId,
+            VolunteerApplySearchCondition condition);
 }
