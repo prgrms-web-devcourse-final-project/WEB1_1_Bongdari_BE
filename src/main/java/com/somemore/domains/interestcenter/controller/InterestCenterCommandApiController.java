ce486aa35cff551130362d481bf0367318465631
leebs0521 <leebs0521@gmail.com>
2025-02-05 17:07:27 +0900

    refactor(interest-center): 관심기관 추가 삭제 리팩토링

--- src/main/java/com/somemore/domains/interestcenter/controller/InterestCenterCommandApiController.java
+++ src/main/java/com/somemore/domains/interestcenter/controller/InterestCenterCommandApiController.java
@@ -4,11 +4,12 @@ import com.somemore.domains.interestcenter.dto.request.RegisterInterestCenterReq
 import com.somemore.domains.interestcenter.dto.response.RegisterInterestCenterResponseDto;
 import com.somemore.domains.interestcenter.usecase.CancelInterestCenterUseCase;
 import com.somemore.domains.interestcenter.usecase.RegisterInterestCenterUseCase;
-import com.somemore.global.auth.annotation.CurrentUser;
+import com.somemore.global.auth.annotation.RoleId;
 import com.somemore.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,8 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,10 +30,11 @@ public class InterestCenterCommandApiController {
     @Operation(summary = "관심기관 등록 API")
     @PostMapping("/api/interest-center")
     public ApiResponse<RegisterInterestCenterResponseDto> registerInterestCenter(
-            @CurrentUser UUID volunteerId,
+            @RoleId UUID volunteerId,
             @Valid @RequestBody RegisterInterestCenterRequestDto requestDto) {
 
-        RegisterInterestCenterResponseDto responseDto = registerInterestCenterUseCase.registerInterestCenter(volunteerId, requestDto);
+        RegisterInterestCenterResponseDto responseDto = registerInterestCenterUseCase.registerInterestCenter(
+                volunteerId, requestDto);
 
         return ApiResponse.ok(200, responseDto, "관심 기관 등록 성공");
     }
@@ -43,7 +43,7 @@ public class InterestCenterCommandApiController {
     @Operation(summary = "관심기관 취소 API")
     @DeleteMapping("/api/interest-center/{centerId}")
     public ApiResponse<String> deleteInterestCenter(
-            @CurrentUser UUID volunteerId,
+            @RoleId UUID volunteerId,
             @PathVariable UUID centerId) {
 
         cancelInterestCenterUseCase.cancelInterestCenter(volunteerId, centerId);
