1e09fa54f0335b7d2472abe13179eaecee4707bc
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-07 14:28:14 +0900

    feat(img): 유저 프로필 이미지 수정을 하나의 엔드포인트로 변경

--- src/main/java/com/somemore/user/dto/request/UpdateProfileImgUrlRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.somemore.user.dto.request;
-
-import java.util.UUID;
-
-public record UpdateProfileImgUrlRequestDto(
-        UUID userId,
-        String profileImgUrl
-) {
-}
--- src/main/java/com/somemore/user/service/UpdateProfileImgUrlService.java
+++ src/main/java/com/somemore/user/service/UpdateProfileImgUrlService.java
@@ -1,25 +1,33 @@
 package com.somemore.user.service;
 
+import com.somemore.global.imageupload.service.PresignedUrl;
+import com.somemore.global.imageupload.usecase.ImageUploadUseCase;
 import com.somemore.user.domain.UserCommonAttribute;
-import com.somemore.user.dto.request.UpdateProfileImgUrlRequestDto;
+import com.somemore.user.dto.request.ImgUrlRequestDto;
 import com.somemore.user.usecase.UpdateProfileImgUrlUseCase;
 import com.somemore.user.usecase.UserQueryUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class UpdateProfileImgUrlService implements UpdateProfileImgUrlUseCase {
 
+    private final ImageUploadUseCase imageUploadUseCase;
     private final UserQueryUseCase userQueryUseCase;
 
     @Override
-    public void updateProfileImgUrl(UpdateProfileImgUrlRequestDto updateProfileImgUrlRequestDto) {
-        UserCommonAttribute userCommonAttribute =
-                userQueryUseCase.getCommonAttributeByUserId(updateProfileImgUrlRequestDto.userId());
+    public String update(UUID userId, ImgUrlRequestDto dto) {
+        PresignedUrl presignedUrl = imageUploadUseCase.getPresignedUrl(dto.fileName());
 
-        userCommonAttribute.updateImgUrl(updateProfileImgUrlRequestDto.profileImgUrl());
+        UserCommonAttribute userCommonAttribute = userQueryUseCase.getCommonAttributeByUserId(userId);
+
+        userCommonAttribute.updateImgUrl(presignedUrl.removeQueryString());
+
+        return presignedUrl.value();
     }
 }
