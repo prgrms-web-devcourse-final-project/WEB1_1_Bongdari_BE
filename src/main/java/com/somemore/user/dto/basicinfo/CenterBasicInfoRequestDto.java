5aef62fd261ca3ab71c2c4d7f7de7d1151da54f8
leebs0521 <leebs0521@gmail.com>
2025-02-10 16:59:37 +0900

    fix(user): 유저 정보 업데이트 오류 수정

--- src/main/java/com/somemore/user/dto/basicinfo/CenterBasicInfoRequestDto.java
+++ src/main/java/com/somemore/user/dto/basicinfo/CenterBasicInfoRequestDto.java
@@ -3,12 +3,14 @@ package com.somemore.user.dto.basicinfo;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record CenterBasicInfoRequestDto(
         @Schema(description = "공통 기본 정보")
-        @NotBlank(message = "공통 기본 정보는 필수 값입니다.")
+        @Valid @NotNull(message = "공통 기본 정보는 필수 값입니다.")
         CommonBasicInfoRequestDto commonBasicInfo,
 
         @Schema(description = "홈페이지 링크")
